e16e68173b26116e59544ec9351163991716c404
vcrespo <victorrcch@hotmail.com>
2019-08-15 12:21:58 -0500

    Cambios creacion del preiliminar de Control de asistencia

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/AsistenciaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    public class AsistenciaController : Controller
+    {
+        // GET: Asistencia
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Asistencia/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asistencia/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asistencia/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asistencia/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asistencia/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asistencia/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asistencia/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/AsistenciaJS/Asistencia.js
@@ -0,0 +1,69 @@
+﻿
+
+function fillBook(valor) {
+    var desSelectEstado = "SelectEstado";
+    var desCheckAsistencia = "CheckAsistencia";
+    var desspanAsistencia = "spanAsistencia";
+    if (valor != 0) {
+        desSelectEstado +=  valor;
+        desCheckAsistencia += valor;
+        desspanAsistencia +=valor;
+    }
+    console.log(desCheckAsistencia);
+    var SelectEstado = document.getElementById(desSelectEstado);
+    var chexkAsistencia = document.getElementById(desCheckAsistencia).checked;
+    var span = document.getElementById(desspanAsistencia);
+
+
+
+    var Hora = new Date().getHours();
+   
+
+//    console.log(chexkAsistencia);
+    if (chexkAsistencia) {
+        if (Hora >= 10) {
+            SelectEstado.selectedIndex = 2;
+            span.style.backgroundColor = "yellow";
+        } else {
+            SelectEstado.selectedIndex = 1;
+            span.style.backgroundColor = "greenyellow";
+        }
+    }
+    else {
+        SelectEstado.selectedIndex = 0;
+        span.style.backgroundColor = "#ccc";
+    }
+}
+
+function CambioEstado(valor) {
+    var desSelectEstado = "SelectEstado";
+    var desCheckAsistencia = "CheckAsistencia";
+    var desspanAsistencia = "spanAsistencia";
+    if (valor != 0) {
+        desSelectEstado += valor;
+        desCheckAsistencia += valor;
+        desspanAsistencia += valor;
+    }
+
+    var SelectEstado = document.getElementById(desSelectEstado).value;
+    var span = document.getElementById(desspanAsistencia);
+    var CheckAsistencia = document.getElementById(desCheckAsistencia);
+
+
+    if (SelectEstado == 1) {
+        CheckAsistencia.checked = true;
+        span.style.backgroundColor = "greenyellow";
+    }
+
+    if (SelectEstado == 2) {
+        CheckAsistencia.checked = true;
+        span.style.backgroundColor = "yellow";
+    }
+
+    if (SelectEstado == 3) {
+        span.style.backgroundColor = "red";
+        CheckAsistencia.checked = true;
+    }
+
+
+}
