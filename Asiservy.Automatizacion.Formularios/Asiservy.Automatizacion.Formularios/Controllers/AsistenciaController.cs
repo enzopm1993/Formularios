6f9a5474e7c65f9f971a3c761cb9cf3a703979ad
prokyo1 <mazza624@gmail.com>
2019-08-19 17:05:38 -0500

    cambio personal

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/AsistenciaController.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/AsistenciaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Asiservy.Automatizacion.Formularios.Models;
 
 namespace Asiservy.Automatizacion.Formularios.Controllers
 {
@@ -48,6 +49,20 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
         public ActionResult PersonalNomina()
         {
             return View();
+        }
+        public ActionResult ConsultarEmpleado()
+        {
+            List<Empleado> Empleados = new List<Empleado>
+            {
+                new Empleado { Cedula = "0940203406", Nombre = "Juan Maldonado" },
+                new Empleado { Cedula = "1188888456", Nombre = "Pedro Suarez" },
+                new Empleado { Cedula = "2723626161", Nombre = "Alejandro Sánchez" },
+                new Empleado { Cedula = "3635261617", Nombre = "María Perez" },
+                new Empleado { Cedula = "1188888456", Nombre = "Andrea Bejarano" },
+                new Empleado { Cedula = "2345789123", Nombre = "Juan Peña" },
+            };
+            return PartialView("Empleados",Empleados);
+
         }
 
     }
