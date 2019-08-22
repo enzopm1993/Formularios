95e5385a75f044f08418476a2fd2664166d6f3d8
vcrespo <victorrcch@hotmail.com>
2019-08-22 16:30:51 -0500

    preliminar -cambio de personal por area - bandejas - login

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/SolicitudPermisoJS/BandejaRRHH.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/SolicitudPermisoJS/BandejaRRHH.js
@@ -2,9 +2,9 @@
 function Observacion() {
     console.log("ok");
     $('#ModalObservacion').modal("show");
-    
+
 }
-function Grabar() {
+function Guardar() {
     Mensaje("Registro Guardado..");
 }
 
@@ -52,10 +52,10 @@ $(document).ready(function () {
 function checkTodos() {
     var i = 1;
     var bool = document.getElementById("checkTodos").checked;
-        console.log('prueba');
+    console.log('prueba');
 
-    $('#TableBandeja tr').each(function () {       
-        var desSol="solicitud"
+    $('#TableBandeja tr').each(function () {
+        var desSol = "solicitud"
         var x = $(this).find("td").eq(1).html();
         console.log(x);
         if (x != null) {
@@ -69,48 +69,3 @@ function Mostrar() {
     $('#ModalAprobacion').modal('toggle')
 }
 
-
-//$(document).ready(function () {
-//    $("#search").keyup(function () {
-//        _this = this;
-//        // Show only matching TR, hide rest of them
-//        $.each($("#WebGrid tbody tr"), function () {
-//            if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-//                $(this).hide();
-//            else
-//                $(this).show();
-//        });
-//    });
-//});
-
-
-
-//    $("body").on("click", ".Grid tfoot a", function () {
-//        $('#WebGridForm').attr('action', $(this).attr('href')).submit();
-//    return false;
-//});
-
-//$('table tbody tr  td').on('click', function () {
-//    $("#myModal").modal("show");
-//    $("#txtSolicitud").val($(this).closest('tr').children()[0].textContent);
-//    $("#txtFecha").val($(this).closest('tr').children()[1].textContent);
-//    $("#txtMotivo").val($(this).closest('tr').children()[2].textContent);
-//    $("#txtArea").val($(this).closest('tr').children()[3].textContent);
-//    $("#txtEmpleado").val($(this).closest('tr').children()[4].textContent);
-
-//    $("#txtSolicitud").prop('disabled', true);
-//    $("#txtFecha").prop('disabled', true);
-//    $("#txtMotivo").prop('disabled', true);
-//    $("#txtArea").prop('disabled', true);
-//    $("#txtEmpleado").prop('disabled', true);
-//});
-//comboFind
-
-
-//function LimpiarTexto() {
-//    $.each($("#TableBandejaRRHH tbody tr"), function () {
-//        $(this).show();
-//    });
-//    document.getElementById("search").innerText = "";
-//    $("#search").val("");
-//}
