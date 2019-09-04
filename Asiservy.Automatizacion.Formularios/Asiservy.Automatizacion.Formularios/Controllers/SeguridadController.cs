7f47546232c910975c3914de1da5d209532618f1
vcrespo <victorrcch@hotmail.com>
2019-09-04 15:36:58 -0500

    seguridad

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/SeguridadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        // GET: Seguridad
+        [Authorize]
+        public ActionResult Opcion()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Rol()
+        {
+            return View();
+        }
+        
+    }
+}
--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/SolicitudPermisoJS/SolicitudPermisoMedico.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/SolicitudPermisoJS/SolicitudPermisoMedico.js
@@ -1,93 +1,93 @@
 ﻿
 
-function CambioLinea(valor) {
-    $("#selectArea").empty();
-    $("#selectArea").append("<option value='' >-- Seleccionar Opción--</option>");
-    $("#selectCargo").empty();
-    $("#selectCargo").append("<option value='' >-- Seleccionar Opción--</option>");
+//function CambioLinea(valor) {
+//    $("#selectArea").empty();
+//    $("#selectArea").append("<option value='' >-- Seleccionar Opción--</option>");
+//    $("#selectCargo").empty();
+//    $("#selectCargo").append("<option value='' >-- Seleccionar Opción--</option>");
 
-    $.ajax({
-        url: "../SolicitudPermiso/ConsultaListadoAreas",
-        type: "Get",
-        data:
-        {
-            CodLinea: valor
-        },
-        success: function (resultado) {
-            if (!$.isEmptyObject(resultado)) {
-                $.each(resultado, function (create, row) {
-                    $("#selectArea").append("<option value='" + row.Codigo + "'>" + row.Descripcion + "</option>")
-                });
-            } else {
-                MensajeAdvertencia("La linea seleccionado no tiene areas asignadas", false);
-            }
-        },
-        error: function (resultado) {
-            MensajeError(JSON.stringify(resultado), false);
-        }
-    });
-}
-
-
-function CambioArea(valor) {
-
-    $("#selectCargo").empty();
-    $("#selectCargo").append("<option value='' >-- Seleccionar Opción--</option>");
-    $.ajax({
-        url: "../SolicitudPermiso/ConsultaListadoCargos",
-        type: "Get",
-        data:
-        {
-            CodArea: valor
-        },
-        success: function (data) {
-            if (!$.isEmptyObject(data)) {
-                $.each(data, function (create, row) {
-                    $("#selectCargo").append("<option value='" + row.Codigo + "'>" + row.Descripcion + "</option>")
-                });
-            } else {
-                MensajeAdvertencia("La linea seleccionado no tiene areas asignadas", false);
-            }
-        },
-        error: function (resultado) {
-            MensajeError(JSON.stringify(resultado), false);
-        }
-    });
-}
+//    $.ajax({
+//        url: "../SolicitudPermiso/ConsultaListadoAreas",
+//        type: "Get",
+//        data:
+//        {
+//            CodLinea: valor
+//        },
+//        success: function (resultado) {
+//            if (!$.isEmptyObject(resultado)) {
+//                $.each(resultado, function (create, row) {
+//                    $("#selectArea").append("<option value='" + row.Codigo + "'>" + row.Descripcion + "</option>")
+//                });
+//            } else {
+//                MensajeAdvertencia("La linea seleccionado no tiene areas asignadas", false);
+//            }
+//        },
+//        error: function (resultado) {
+//            MensajeError(JSON.stringify(resultado), false);
+//        }
+//    });
+//}
 
 
+//function CambioArea(valor) {
+
+//    $("#selectCargo").empty();
+//    $("#selectCargo").append("<option value='' >-- Seleccionar Opción--</option>");
+//    $.ajax({
+//        url: "../SolicitudPermiso/ConsultaListadoCargos",
+//        type: "Get",
+//        data:
+//        {
+//            CodArea: valor
+//        },
+//        success: function (data) {
+//            if (!$.isEmptyObject(data)) {
+//                $.each(data, function (create, row) {
+//                    $("#selectCargo").append("<option value='" + row.Codigo + "'>" + row.Descripcion + "</option>")
+//                });
+//            } else {
+//                MensajeAdvertencia("La linea seleccionado no tiene areas asignadas", false);
+//            }
+//        },
+//        error: function (resultado) {
+//            MensajeError(JSON.stringify(resultado), false);
+//        }
+//    });
+//}
 
 
-function CambioHoraFecha() {
-    var HoraDesde = document.getElementById("timeHoraSalida");
-    var HoraHasta = document.getElementById("timeHoraRegreso");
-    var FechaSalidaRegreso = document.getElementById("dateSalidaRegreso");
-    var FechaDesde = document.getElementById("dateSalida");
-    var FechaHasta = document.getElementById("dateRegreso");
-    var check = document.getElementById("switchHoraFecha").checked
-    console.log(check);
 
-    if (check) {
-        HoraDesde.removeAttribute("readonly");
-        HoraHasta.removeAttribute("readonly");
-        FechaSalidaRegreso.removeAttribute("readonly");
-        FechaDesde.setAttribute("readonly", true);
-        FechaHasta.setAttribute("readonly", true);
-        console.log(FechaDesde);
-        FechaDesde.value = null;
-        FechaHasta.value = null;
-    } else {
 
-        HoraDesde.setAttribute("readonly", true);
-        HoraHasta.setAttribute("readonly", true);
-        FechaSalidaRegreso.setAttribute("readonly", true);
-        FechaDesde.removeAttribute("readonly");
-        FechaHasta.removeAttribute("readonly");
-        FechaSalidaRegreso.value = null;
-        HoraDesde.value = null;
-        HoraHasta.value = null;
-    }
-}
+//function CambioHoraFecha() {
+//    var HoraDesde = document.getElementById("timeHoraSalida");
+//    var HoraHasta = document.getElementById("timeHoraRegreso");
+//    var FechaSalidaRegreso = document.getElementById("dateSalidaRegreso");
+//    var FechaDesde = document.getElementById("dateSalida");
+//    var FechaHasta = document.getElementById("dateRegreso");
+//    var check = document.getElementById("switchHoraFecha").checked
+//    console.log(check);
+
+//    if (check) {
+//        HoraDesde.removeAttribute("readonly");
+//        HoraHasta.removeAttribute("readonly");
+//        FechaSalidaRegreso.removeAttribute("readonly");
+//        FechaDesde.setAttribute("readonly", true);
+//        FechaHasta.setAttribute("readonly", true);
+//        console.log(FechaDesde);
+//        FechaDesde.value = null;
+//        FechaHasta.value = null;
+//    } else {
+
+//        HoraDesde.setAttribute("readonly", true);
+//        HoraHasta.setAttribute("readonly", true);
+//        FechaSalidaRegreso.setAttribute("readonly", true);
+//        FechaDesde.removeAttribute("readonly");
+//        FechaHasta.removeAttribute("readonly");
+//        FechaSalidaRegreso.value = null;
+//        HoraDesde.value = null;
+//        HoraHasta.value = null;
+//    }
+//}
 
 
 $("#selectSubGrupoEnfermedad").change(function () {
