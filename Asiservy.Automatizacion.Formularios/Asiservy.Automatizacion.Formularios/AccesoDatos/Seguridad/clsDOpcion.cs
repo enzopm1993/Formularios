b945f71dcbd1877f33a00c1449d6e42191804280
vcrespo <victorrcch@hotmail.com>
2019-09-04 16:01:12 -0500

    seguridad

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Seguridad/clsDOpcion.cs
@@ -0,0 +1,21 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Seguridad
+{
+    public class clsDOpcion
+    {
+
+        public List<OPCION> ConsultarOpciones()
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                return null;
+            }
+        }
+
+    }
+}
