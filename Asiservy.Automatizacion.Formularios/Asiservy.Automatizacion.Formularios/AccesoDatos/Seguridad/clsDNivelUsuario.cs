cc0c416b835f14788ad2157775feedbfd7391f73
Ing Victor Crespo <victorrcch@hotmail.com>
2019-09-06 15:31:39 -0500

    Seguridad:  NivelUsuario

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Seguridad/clsDNivelUsuario.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Seguridad/clsDNivelUsuario.cs
@@ -9,21 +9,16 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Seguridad
 {
     public class clsDNivelUsuario
     {
+        clsDEmpleado clsDEmpleado = null;
         public List<NivelUsuarioViewModel> ConsultarNivelUsuario(NIVEL_USUARIO filtros)
         {
             using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
             {
                 var query = from o in entities.NIVEL_USUARIO select o;
-                clsDEmpleado clsDEmpleado = new clsDEmpleado();
+                 clsDEmpleado = new clsDEmpleado();
                 List<NivelUsuarioViewModel> Lista = new List<NivelUsuarioViewModel>();
-                var Empleados = clsDEmpleado.ConsultaEmpleado(filtros.IdUsuario);
-
-                if (!string.IsNullOrEmpty(filtros.EstadoRegistro))
-                {
-                    query = query.Where(x => x.EstadoRegistro == filtros.EstadoRegistro);
-                }
-
-                    Lista = (from q in query.ToList()
+                var Empleados = clsDEmpleado.ConsultaEmpleado("0");
+                Lista = (from q in query.ToList()
                             join e in Empleados on q.IdUsuario equals e.CEDULA
                             select new NivelUsuarioViewModel
                             {
