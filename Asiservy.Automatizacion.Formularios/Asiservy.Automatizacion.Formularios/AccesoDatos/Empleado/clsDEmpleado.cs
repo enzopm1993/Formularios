79a9a1635831f652807b6fa9b9011abc2468a80d
Ing Victor Crespo <victorrcch@hotmail.com>
2019-09-18 14:08:51 -0500

    Empleado Turno

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Empleado/clsDEmpleado.cs
@@ -0,0 +1,200 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using Asiservy.Automatizacion.Formularios.Models.Empleado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos
+{
+    public class clsDEmpleado
+    {
+
+        public void GuardarModificarEmpleadoTurno(EmpleadoViewModel model)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                var EmpleadoTurno = db.EMPLEADO_TURNO.FirstOrDefault(X=> X.Cedula == model.Cedula);
+                if(EmpleadoTurno!= null)
+                {
+                    EmpleadoTurno.Turno = model.Turno;
+                    EmpleadoTurno.CodLinea = model.CodLinea;
+                    EmpleadoTurno.FechaModificacionLog = DateTime.Now;
+                    EmpleadoTurno.EstadoRegistro = model.EstadoRegistro;
+                    EmpleadoTurno.UsuarioModificacionLog = model.UsuarioIngreso;
+                    EmpleadoTurno.TerminalModificacionLog = model.TerminalIngreso;
+
+                    db.BITACORA_EMPLEADO_TURNO.Add(new BITACORA_EMPLEADO_TURNO
+                    {
+                        Cedula = model.Cedula,
+                        CodLinea = model.CodLinea,
+                        EstadoRegistro = clsAtributos.EstadoRegistroActivo,
+                        FechaIngresoLog = DateTime.Now,
+                        Observacion = "Asigna Turno",
+                        TerminalIngresoLog = model.TerminalIngreso,
+                        Turno = model.Turno,
+                        UsuarioIngresoLog = model.UsuarioIngreso
+                    });
+                }
+                else
+                {
+                    db.EMPLEADO_TURNO.Add(new EMPLEADO_TURNO
+                    {
+                        Cedula= model.Cedula,
+                        Turno= model.Turno,
+                        CodLinea = model.CodLinea,
+                        EstadoRegistro =model.EstadoRegistro,
+                        FechaIngresoLog = model.FechaIngreso,
+                        UsuarioIngresoLog = model.UsuarioIngreso,
+                        TerminalIngresoLog = model.TerminalIngreso
+                    });
+                    db.BITACORA_EMPLEADO_TURNO.Add(new BITACORA_EMPLEADO_TURNO
+                    {
+                        Cedula = model.Cedula,
+                        CodLinea = model.CodLinea,
+                        EstadoRegistro = clsAtributos.EstadoRegistroActivo,
+                        FechaIngresoLog = DateTime.Now,
+                        Observacion = "Asigna Turno",
+                        TerminalIngresoLog = model.TerminalIngreso,
+                        Turno = model.Turno,
+                        UsuarioIngresoLog = model.UsuarioIngreso
+                    });
+                }
+                db.SaveChanges();
+
+            }
+        }
+
+        public List<EmpleadoViewModel> ConsultaEmpleadoTurno(string Linea)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                List<EmpleadoViewModel> ListaEmpleadoTurno = new List<EmpleadoViewModel>();
+
+                var poEmpleadoTurno = db.spConsutaEmpleadosTurnos(Linea).ToList();
+
+                foreach(var x in poEmpleadoTurno)
+                {
+                    ListaEmpleadoTurno.Add(new EmpleadoViewModel {
+                        Cedula = x.CEDULA,
+                        Nombre = x.NOMBRES,
+                        CodLinea=x.CODLINEA,
+                        Linea = x.LINEA,
+                         Turno = x.TURNO
+                    });
+                }
+                return ListaEmpleadoTurno;
+            }
+        }
+
+        public List<spConsutaReporteEmpleadosTurnos> ConsultaReporteEmpleadoTurno(string Linea, string Turno)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {              
+                List<spConsutaReporteEmpleadosTurnos> poEmpleadoTurno = db.spConsutaReporteEmpleadosTurnos(Linea,Turno).ToList();   
+                
+                return poEmpleadoTurno;
+            }
+        }
+
+        public void ActualizarEmpleadoTurno(EMPLEADO_TURNO model)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                var poEmpleadoTurno = db.EMPLEADO_TURNO.FirstOrDefault(x=> x.Cedula == model.Cedula);
+                if (poEmpleadoTurno != null)
+                {
+                    poEmpleadoTurno.CodLinea = model.CodLinea;
+                    poEmpleadoTurno.Turno = "1";
+                    poEmpleadoTurno.FechaModificacionLog = DateTime.Now;
+                    poEmpleadoTurno.UsuarioModificacionLog = model.UsuarioIngresoLog;
+                    poEmpleadoTurno.TerminalModificacionLog = model.TerminalModificacionLog;
+
+                    db.BITACORA_EMPLEADO_TURNO.Add(new BITACORA_EMPLEADO_TURNO
+                    {
+                        Cedula = model.Cedula,
+                        CodLinea = model.CodLinea,
+                        EstadoRegistro = clsAtributos.EstadoRegistroActivo,
+                        FechaIngresoLog = DateTime.Now,
+                        Observacion = "Cambio de Area",
+                        TerminalIngresoLog = model.TerminalIngresoLog,
+                        Turno = "1",
+                        UsuarioIngresoLog = model.UsuarioIngresoLog
+                    });
+
+
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public List<spConsutaEmpleadosFiltro> ConsultaEmpleadosFiltro(string dsLinea, string dsArea, string dsCargo)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+
+                List<spConsutaEmpleadosFiltro> pListEmpleados = null;
+                if (string.IsNullOrEmpty(dsLinea))
+                    dsLinea = "0";
+                if (string.IsNullOrEmpty(dsArea))
+                    dsArea = "0";
+                if (string.IsNullOrEmpty(dsCargo))
+                    dsCargo = "0";
+
+                pListEmpleados = db.spConsutaEmpleadosFiltro(dsArea, dsLinea, dsCargo).ToList();
+                return pListEmpleados;
+            }
+        }
+
+        public List<spConsutaEmpleadosFiltro> ConsultaEmpleadosFiltroCambioPersonal(string dsLinea, string dsArea, string dsCargo,string psTipo)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                if (string.IsNullOrEmpty(dsLinea))
+                    dsLinea = "0";
+                if (string.IsNullOrEmpty(dsArea))
+                    dsArea = "0";
+                if (string.IsNullOrEmpty(dsCargo))
+                    dsCargo = "0";
+
+                List<spConsutaEmpleadosFiltro> pListEmpleados = null;
+                List<spConsutaEmpleadosFiltro> pListEmpleadoR = new List<spConsutaEmpleadosFiltro>();
+                pListEmpleados = db.spConsutaEmpleadosFiltro(dsArea, dsLinea, dsCargo).ToList();
+                if (psTipo == clsAtributos.TipoPrestar)
+                {
+                    foreach (var item in pListEmpleados.ToArray())
+                    {
+                        if (db.CAMBIO_PERSONAL.Any(x => x.Cedula == item.CEDULA&&x.EstadoRegistro==clsAtributos.EstadoRegistroActivo))
+                        {
+                            pListEmpleados.Remove(item);
+                        }
+                    }
+                    return pListEmpleados;
+                }
+                else
+                {
+                    foreach (var item in pListEmpleados)
+                    {
+                        if (db.CAMBIO_PERSONAL.Any(x => x.Cedula == item.CEDULA&&x.EstadoRegistro==clsAtributos.EstadoRegistroActivo))
+                        {
+                            pListEmpleadoR.Add(item);
+                        }
+                    }
+                    return pListEmpleadoR;
+                }
+                
+                
+            }
+        }
+        public List<spConsutaEmpleados> ConsultaEmpleado(string dsCedula)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())            {
+
+                List<spConsutaEmpleados> pListEmpleados = null;
+                if (!string.IsNullOrEmpty(dsCedula))
+                    pListEmpleados = db.spConsutaEmpleados(dsCedula).ToList();
+                return pListEmpleados;
+            }
+        }
+    }
+}
--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/EmpleadoController.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/EmpleadoController.cs
@@ -1,8 +1,10 @@
 ﻿using Asiservy.Automatizacion.Datos.Datos;
 using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using Asiservy.Automatizacion.Formularios.Models.Empleado;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,21 +13,32 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
     public class EmpleadoController : Controller
     {
 
-        string[] liststring = null;
+        string[] Usuario = null;
         clsDError clsDError = null;
+        clsDEmpleado clsDEmpleado = null;
+        
         // GET: Empleado
+        [Authorize]
         public ActionResult EmpleadoTurno()
-        {
-            
+        {            
             try
             {
-                return View();
+                Usuario = User.Identity.Name.Split('_');
+                clsDEmpleado = new clsDEmpleado();
+                var Empleado = clsDEmpleado.ConsultaEmpleado(Usuario[1]).FirstOrDefault();
+                List<EmpleadoViewModel> model = new List<EmpleadoViewModel>();
+                ViewBag.Linea = Empleado.LINEA;
+                if (Empleado != null)
+                {
+                     model = clsDEmpleado.ConsultaEmpleadoTurno(Empleado.CODIGOLINEA);
+                }
+                return View(model);
             }
             catch (Exception ex)
             {
 
                 SetErrorMessage(ex.Message);
-                liststring = User.Identity.Name.Split('_');
+                Usuario = User.Identity.Name.Split('_');
                 clsDError = new clsDError();
                 clsDError.GrabarError(new ERROR
                 {
@@ -34,12 +47,118 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
                     Observacion = "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(),
                     FechaIngreso = DateTime.Now,
                     TerminalIngreso = Request.UserHostAddress,
-                    UsuarioIngreso = liststring[1]
+                    UsuarioIngreso = Usuario[1]
                 });
-                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+                return View();
             }
            
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult EmpleadoTurno(List<EmpleadoViewModel> model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) { return View(model); }
+
+                Usuario = User.Identity.Name.Split('_');
+                clsDEmpleado = new clsDEmpleado();
+                //var Empleado = clsDEmpleado.ConsultaEmpleado(Usuario[1]).FirstOrDefault();
+                // List<EmpleadoViewModel> model = new List<EmpleadoViewModel>();
+                foreach(var x in model)
+                {
+                    x.EstadoRegistro = clsAtributos.EstadoRegistroActivo;
+                    x.FechaIngreso = DateTime.Now;
+                    x.TerminalIngreso = Request.UserHostAddress;
+                    x.UsuarioIngreso = Usuario[0];
+                    clsDEmpleado.GuardarModificarEmpleadoTurno(x);
+                }
+                SetSuccessMessage(clsAtributos.MsjRegistroGuardado);
+                return RedirectToAction("EmpleadoTurno");
+            }
+            catch (Exception ex)
+            {
+
+                SetErrorMessage(ex.Message);
+                Usuario = User.Identity.Name.Split('_');
+                clsDError = new clsDError();
+                clsDError.GrabarError(new ERROR
+                {
+                    Controlador = this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    Mensaje = ex.Message,
+                    Observacion = "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(),
+                    FechaIngreso = DateTime.Now,
+                    TerminalIngreso = Request.UserHostAddress,
+                    UsuarioIngreso = Usuario[1]
+                });
+                return RedirectToAction("EmpleadoTurno");
+            }
+
+        }
+
+        public ActionResult ReporteEmpleadoTurno()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                SetErrorMessage(ex.Message);
+                Usuario = User.Identity.Name.Split('_');
+                clsDError = new clsDError();
+                clsDError.GrabarError(new ERROR
+                {
+                    Controlador = this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    Mensaje = ex.Message,
+                    Observacion = "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(),
+                    FechaIngreso = DateTime.Now,
+                    TerminalIngreso = Request.UserHostAddress,
+                    UsuarioIngreso = Usuario[1]
+                });
+                return RedirectToAction("Home","Home");
+            }
+        }
+
+        public ActionResult ReporteEmpleadoTurnoPartial(string dsTurno)
+        {
+            try
+            {
+                clsDEmpleado = new clsDEmpleado();
+                Usuario = User.Identity.Name.Split('_');
+                List<spConsutaReporteEmpleadosTurnos> model = new List<spConsutaReporteEmpleadosTurnos>();
+                var empleado = clsDEmpleado.ConsultaEmpleado(Usuario[1]).FirstOrDefault();
+                if (empleado != null)
+                {
+                    model = clsDEmpleado.ConsultaReporteEmpleadoTurno(empleado.CODIGOLINEA,dsTurno);
+                }
+
+                return PartialView(model);
+            }
+            catch (Exception ex)
+            {
+
+                //SetErrorMessage(ex.Message);
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                Usuario = User.Identity.Name.Split('_');
+                clsDError = new clsDError();
+                clsDError.GrabarError(new ERROR
+                {
+                    Controlador = this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    Mensaje = ex.Message,
+                    Observacion = "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(),
+                    FechaIngreso = DateTime.Now,
+                    TerminalIngreso = Request.UserHostAddress,
+                    UsuarioIngreso = Usuario[1]
+                });
+                return Json(ex.Message,JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+
         protected void SetSuccessMessage(string message)
         {
             TempData["MensajeConfirmacion"] = message;
