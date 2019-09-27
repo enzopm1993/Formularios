e70b081f4f2c18ac1d521c0ea26af9854a64243f
Ing Victor Crespo <victorrcch@hotmail.com>
2019-09-27 12:09:29 -0500

    modelo

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/PROYECCION_PROGRAMACION.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/PROYECCION_PROGRAMACION.cs
@@ -30,5 +30,6 @@ namespace Asiservy.Automatizacion.Datos.Datos
         public Nullable<System.DateTime> FechaModificacionLog { get; set; }
         public string UsuarioModificacionLog { get; set; }
         public string TerminalModificacionLog { get; set; }
+        public string Observacion { get; set; }
     }
 }
