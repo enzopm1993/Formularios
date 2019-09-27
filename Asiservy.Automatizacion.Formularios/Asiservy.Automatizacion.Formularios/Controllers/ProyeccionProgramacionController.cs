80b1a3479d89f7fb2f1e89e7534f5e831a986bf6
prokyo1 <mazza624@gmail.com>
2019-09-27 11:14:57 -0500

    proyeccion programacion

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/ProyeccionProgramacionController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    public class ProyeccionProgramacionController : Controller
+    {
+        // GET: ProyeccionProgramacion
+        [Authorize]
+        public ActionResult ProyeccionProgramacion()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        [Authorize]
+        public ActionResult ProyeccionProgramacionPartial()
+        {
+            try
+            {
+                return PartialView();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        [Authorize]
+        public ActionResult ProyeccionProgramacionEditar()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
