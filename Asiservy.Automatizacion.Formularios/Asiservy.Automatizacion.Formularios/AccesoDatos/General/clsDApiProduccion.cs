d9224a2d3aeb570de477a3b251e8e5589e782ed9
vcrespo <desarrollo1@DESARROLLO01>
2019-10-15 12:18:22 -0500

    control de coches api de tallas

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/General/clsDApiProduccion.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.General
+{
+    public class clsDApiProduccion
+    {
+        public object ConsultarTallas(string Talla)
+        {
+            var client = new RestClient("http://192.168.0.31:8870");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            RestRequest request;
+            if (string.IsNullOrEmpty(Talla))
+             request = new RestRequest("/api/Produccion/Tallas", Method.GET);
+            else
+             request = new RestRequest("/api/Produccion/Tallas/"+Talla+"/", Method.GET);
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            var ListaTallas = JsonConvert.DeserializeObject(content);
+            return ListaTallas;
+
+        }
+    }
+}
