de00091920d65e7ee4467fb72b94aaf6cad200e2
vcrespo <desarrollo1@DESARROLLO01>
2019-10-29 12:34:31 -0500

    nuevo proyecto, wcf servicios de formularios

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Servicios/IServiceFormularios.cs
@@ -0,0 +1,60 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Asiservy.Automatizacion.Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServiceFormularios
+    {
+
+        [OperationContract]
+        RespuestaGenerica GenerarSolicitudPermiso(string Identificacion, string CodigoMotivo, string Observacion, string UsuarioIngreso, string TerminalIngreso, DateTime FechaSalida, DateTime FechaRegreso);
+       
+
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "Asiservy.Automatizacion.Servicios.ContractType".
+    [DataContract]
+    public class RespuestaGenerica
+    {
+        public bool Respuesta { get; set; }
+        public string Mensaje { get; set; }
+
+    }
+
+
+    //    [DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
+
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
+
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
+}
