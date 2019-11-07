8391e28e41ab7cb68437573d2a1ff28d9b9861ce
prokyo1 <mazza624@gmail.com>
2019-11-07 14:22:57 -0500

    general js

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/General/GeneralJS.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/General/GeneralJS.js
@@ -1,4 +1,16 @@
-﻿
+﻿function sololetras(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+
+    //Tecla de retroceso para borrar, siempre la permite
+    if (tecla == 8 || tecla == 32 || tecla == 13) {
+        return true;
+    }
+
+    // Patron de entrada, en este caso solo acepta numeros y letras
+    patron = /[A-Za-z0-9]/;
+    tecla_final = String.fromCharCode(tecla);
+    return patron.test(tecla_final);
+}
 
 function MostrarModalCargando() {
 
