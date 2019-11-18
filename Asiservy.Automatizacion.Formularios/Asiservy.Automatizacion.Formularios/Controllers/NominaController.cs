b2d375445bf64cf6c773debae7ab7d30376769ab
aintriago <aintriago@asiservy.com>
2019-11-18 15:26:33 -0500

    Empleados SAP

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/NominaController.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/NominaController.cs
@@ -1,12 +1,17 @@
 ﻿using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Asiservy.Automatizacion.Formularios.Controllers
 {
+    [Authorize]
     public class NominaController : Controller
     {
 
@@ -40,5 +45,53 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
             }
         }
 
+        public ActionResult ListaEmpleadosPartial()
+        {
+            try
+            {
+
+                var client = new RestClient("http://192.168.0.31:8870");
+                var request = new RestRequest("/api/Empleado/SapClientes", Method.GET);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+                var ListaEmpleados = JsonConvert.DeserializeObject<List<clsEmpleadoCliente>>(content);
+                return PartialView(ListaEmpleados);
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+    }
+
+    public class clsEmpleadoCliente
+    {
+        public string CODEMPLEADO { get; set; }
+        public string NOMBRE_EMPLEADO { get; set; }
+        public string CEDULA { get; set; }
+        public string GENERO { get; set; }
+        public string DEPARTAMENTO { get; set; }
+        public string CARGO { get; set; }
+        public string LINEA { get; set; }
+        public string RECURSO { get; set; }
+        public string CODIGO_SAP { get; set; }
+        public bool EXISTE_SAP { get; set; }
     }
 }
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Nomina/EmpleadosClientes.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    CargarOpciones();
+});
+function CargarOpciones() {
+    $.ajax({
+        url: "../Nomina/ListaEmpleadosPartial",
+        type: "GET",
+        success: function (resultado) {
+
+            var contenidoTabla = $('#DivTableEmpleadosClientes');
+            contenidoTabla.html('');
+            contenidoTabla.html(resultado);
+            config.opcionesDT.pageLength = 100;
+            config.opcionesDT.order = [[1, "asc"]];
+            $('#tblDataTable').DataTable(config.opcionesDT);
+
+        },
+        error: function (resultado) {
+            MensajeError(resultado, false);
+        }
+    });
+}
