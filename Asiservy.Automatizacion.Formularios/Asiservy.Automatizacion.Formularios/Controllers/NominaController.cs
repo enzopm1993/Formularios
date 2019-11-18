66bf439834ebedf86f2147fe7884c52d014be103
aintriago <aintriago@asiservy.com>
2019-11-18 08:23:05 -0500

    Sección Nómina

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/NominaController.cs
@@ -0,0 +1,44 @@
+﻿using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    public class NominaController : Controller
+    {
+
+       
+        clsDError clsDError = null;
+        string[] lsUsuario;
+
+        protected void SetErrorMessage(string message)
+        {
+            TempData["MensajeError"] = message;
+        }
+
+        public ActionResult EmpleadosClientes()
+        {
+            try
+            {
+                ViewBag.dataTableJS = "1";
+                ViewBag.JavaScrip = RouteData.Values["controller"] + "/" + RouteData.Values["action"];
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+        }
+
+    }
+}
