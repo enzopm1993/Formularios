0bda490e182c1ab179f0f16fc2ddfeea0f97a38e
vcrespo <victorrcch@hotmail.com>
2019-11-21 18:01:27 -0500

    modelo

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/spConsultaProyeccionProgramacion.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/spConsultaProyeccionProgramacion.cs
@@ -44,6 +44,7 @@ namespace Asiservy.Automatizacion.Datos.Datos
         public Nullable<System.DateTime> FechaModificacionLog { get; set; }
         public string UsuarioModificacionLog { get; set; }
         public string TerminalModificacionLog { get; set; }
+        public string CodBarco { get; set; }
         public string Barco { get; set; }
         public Nullable<int> CodMarea { get; set; }
         public string Marea { get; set; }
