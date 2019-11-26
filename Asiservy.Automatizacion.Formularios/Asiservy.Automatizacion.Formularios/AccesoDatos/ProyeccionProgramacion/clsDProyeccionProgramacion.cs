82875b444df786f0b682a3f0d2f2752098a0cda1
vcrespo <victorrcch@hotmail.com>
2019-11-26 11:53:05 -0500

    ajustes

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/ProyeccionProgramacion/clsDProyeccionProgramacion.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/ProyeccionProgramacion/clsDProyeccionProgramacion.cs
@@ -242,7 +242,7 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.ProyeccionProgramacion
                         detalle.TemperaturaFinal = model.TemperaturaFinal;
                         detalle.Observacion = model.Observacion != null ? model.Observacion.ToUpper() : "";
                         detalle.RecetaRoceado = model.RecetaRoceado;
-                        detalle.Lote = model.Lote;
+                        detalle.Lote = model.Lote.ToUpper();
                     }
 
                     detalle.UsuarioModificacionLog = model.UsuarioIngresoLog;
