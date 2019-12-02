63ba183805aab7d82fb40c7e69522322ee0eb60e
vcrespo <victorrcch@hotmail.com>
2019-12-02 16:26:04 -0500

    modelo

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/CONTROL_MATERIAL_DETALLE.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/CONTROL_MATERIAL_DETALLE.cs
@@ -27,6 +27,7 @@ namespace Asiservy.Automatizacion.Datos.Datos
         public Nullable<System.DateTime> FechaModificacionLog { get; set; }
         public string UsuarioModificacionLog { get; set; }
         public string TerminalModificacionLog { get; set; }
+        public Nullable<int> DadoBaja { get; set; }
     
         public virtual CONTROL_MATERIAL CONTROL_MATERIAL { get; set; }
     }
