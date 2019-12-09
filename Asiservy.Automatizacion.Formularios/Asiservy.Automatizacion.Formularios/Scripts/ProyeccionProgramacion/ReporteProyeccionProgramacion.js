d7a2f4010b04c70aa825b07381a4e12a64fb94ba
vcrespo <victorrcch@hotmail.com>
2019-12-09 16:46:17 -0500

    ajustes de reporte de proyeccion de la programacion

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/ProyeccionProgramacion/ReporteProyeccionProgramacion.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/ProyeccionProgramacion/ReporteProyeccionProgramacion.js
@@ -4,6 +4,42 @@ $(document).ready(function () {
     
 });
 
+function ValidarEstado() {
+  //  console.log("Hola mundo");
+    $("#Check1").prop("checked", false);
+    $("#Check2").prop("checked", false);
+    $("#Check3").prop("checked", false);
+    $("#Check4").prop("checked", false);
+    $("#Check5").prop("checked", false);
+    $.ajax({
+        url: "../ProyeccionProgramacion/ValidarProyeccionProgramacionEstado",
+        type: "GET",
+        data: { Fecha: $('#txtFecha').val() },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }            
+            if (resultado == 1) {
+                $("#Check1").prop("checked", true);
+            } else if (resultado == 2) {
+                $("#Check2").prop("checked", true);
+            } else if (resultado == 3) {
+                $("#Check3").prop("checked", true);
+            } else if (resultado == 4) {
+                $("#Check4").prop("checked", true);
+            } else if (resultado == 5) {
+                $("#Check5").prop("checked", true);
+            }
+
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText);
+            $("#spinnerCargando").prop("hidden", true);
+
+        }
+    });
+}
+
 function CargarReporte() {
 
     if ($('#txtFecha').val() == "") {
@@ -25,6 +61,7 @@ function CargarReporte() {
             $("#spinnerCargando").prop("hidden", true);
             config.opcionesDT.pageLength = 50;
             $('#tblDataTable').DataTable(config.opcionesDT);
+            ValidarEstado();
 
         },
         error: function (resultado) {
