fa42582ab9f2cd47d08b2e80738b83ff0e069a38
prokyo1 <mazza624@gmail.com>
2020-01-02 14:08:32 -0500

    reporte asistencia

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Asistencia/RptAsistencia.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Asistencia/RptAsistencia.js
@@ -5,6 +5,19 @@
     } else {
         $('#msjerrorFecha1').hide();
     }
+    if ($('#Linea').val() == '') {
+        $('#msjerrorLinea').show();
+        return false;
+    } else {
+        $('#msjerrorLinea').hide();
+    }
+    if ($('#Turno').val() == '') {
+        $('#msjerrorTurno').show();
+        return false;
+    } else {
+        $('#msjerrorTurno').hide();
+    }
+    $("#spinnerCargando").prop("hidden", false);
     $.ajax({
         url: "../Asistencia/RptAsistenciaPartial",
         type: "GET",
@@ -16,7 +29,7 @@
             Turno: $('#Turno').val()
         },
         success: function (resultado) {
-           
+            $("#spinnerCargando").prop("hidden", true);
             $('#divRptAsistencia').html(resultado);
             //MensajeCorrecto("Registro ingresado con éxito", false);
         },
