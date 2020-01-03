15c7b5b78ea27266fe70665f9db06f4145834b5d
prokyo1 <mazza624@gmail.com>
2020-01-03 08:28:40 -0500

    movimiento personal

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/NominaController.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/NominaController.cs
@@ -1,5 +1,6 @@
 ﻿using Asiservy.Automatizacion.Formularios.AccesoDatos;
 using Asiservy.Automatizacion.Formularios.AccesoDatos.App;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.Asistencia;
 using Asiservy.Automatizacion.Formularios.AccesoDatos.Nomina;
 using Newtonsoft.Json;
 using RestSharp;
@@ -20,6 +21,7 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
 
        
         clsDError clsDError = null;
+        clsDAsistencia ClsdAsistencia = null;
         string[] lsUsuario;
 
         protected void SetErrorMessage(string message)
@@ -312,12 +314,27 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
             ClsKeyValue obReturn = new ClsKeyValue();
             using (DataLifeService.ServicioAsiservySoapClient servicio = new DataLifeService.ServicioAsiservySoapClient())
             {
+                List<string> arrCedulas=new List<string>();
                 DataSet content;
                 DataTable dt;
                 string codigoReturn;
                 string msgReturn;
                 List<Respuesta> Respuesta=new List<Respuesta>();
-                foreach (var cedula in parametros.Cedula)
+                ClsdAsistencia = new clsDAsistencia();
+                foreach (var item in parametros.Cedula)
+                {
+                    if (ClsdAsistencia.CosultarAsistenciaEmpleado(item, DateTime.Now))
+                    {
+                        //remover cedula y agregar msjerror
+                        Respuesta.Add(new Respuesta { cedula = item, Codigo = "001", Descripcion = "No se puede mover, La asistencia ya habia sido generada" });
+                    }
+                    else
+                    {
+                        arrCedulas.Add(item);
+                    }
+                }
+                //foreach (var cedula in parametros.Cedula)
+                foreach (var cedula in arrCedulas.ToArray())
                 {
                     content = servicio.actualizarCodigosEmpleados(cedula, parametros.Compania, parametros.CentroCostos, parametros.Cargo, parametros.Linea, parametros.Recurso);
                     dt = content.Tables[0];
@@ -327,9 +344,8 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
                     obReturn.Codigo = codigoReturn;
                     Respuesta.Add(new Respuesta { cedula = cedula, Codigo = obReturn.Codigo, Descripcion = obReturn.Descripcion });
                 }
-                
-               
-                return Json(obReturn, JsonRequestBehavior.AllowGet);
+                //return Json(obReturn, JsonRequestBehavior.AllowGet);
+                return Json(Respuesta, JsonRequestBehavior.AllowGet);
             }
 
         }
--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Asistencia/MoverPersonalDataLife.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Asistencia/MoverPersonalDataLife.js
@@ -32,6 +32,48 @@
         MensajeAdvertencia("Debe seleccionar al menos el centro de costos a consultar", false);
 
     }
+}
+function MoverEmpleados() {
+    i = 0; 
+    var result = new Array();
+    //ConsultarEmpleado = "ConsultarEmpleado";
+    if ($('#selectLinea').val() != "" && $('#SelectArea').val() != "" && $('#SelectRecurso').val() != "" && $('#SelectCargo').val() != "") {
+        $("input[type=checkbox]:checked").each(function (resultado) {
+            id = $(this).attr("id");
+            this.id = id.replace('Empleado-', '');
+            result.push(this.id);
+            i++;
+        });
+    } else {
+        MensajeAdvertencia("Centro de Costos, Recurso, Línea,Cargo,fecha y hora son obligatorios", false);
+    }
+        MostrarModalCargando();
+        $.ajax({
+            url: "../Nomina/ActualizaEmpleadosArea",
+            type: "POST",
+            data:
+            {
+                //pslinea: $('#SelectLineaOrigen').val(),
+                //psarea: $('#SelectAreaOrigen').val(),
+                //pscargo: $('#SelectCargoOrigen').val(),
+                //tipo: $('#optcambiaremp').val()
+                Cedula: result,
+                CentroCostos: $('#SelectArea').val(),
+                Recurso: $('#SelectRecurso').val(),
+                Linea: $('#SelectLinea').val(),
+                Cargo: $('#SelectCargo').val(),
+            },
+            success: function (data) {
+                //$('#DivEmpleados').html(data);
+                //$('#btnGuardarCambioEmp').show();
+                //$('#Guardar').show();
+                //$('#Guardar').val('Mover Empleados');
+                CerrarModalCargando();
+                MensajeCorrecto("Empleados movidos con éxito", true);
+                
 
-
+            }
+        });
+        $('#contempleados').show();
+   
 }
