6a6e78abd6c2e8103b183c1c7f76c7a4855996d8
vcrespo <victorrcch@hotmail.com>
2020-01-27 11:29:50 -0500

    avance de entrega de uniformes

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/EntregaUniforme/clsDEntregaUniforme.cs
@@ -0,0 +1,61 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using Asiservy.Automatizacion.Formularios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.EntregaUniforme
+{
+    public class clsDEntregaUniforme
+    {
+        public List<spConsultaUniformeEntregar> ConsultaEntregaUniforme(DateTime Fecha)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var lista = entities.spConsultaUniformeEntregar(Fecha).ToList();
+                return lista;
+            }
+        }
+        public RespuestaGeneral GuardarModificarControl(ENTREGA_UNIFORME control)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var result = entities.ENTREGA_UNIFORME.FirstOrDefault(x => x.IdEntregaUniforme == control.IdEntregaUniforme);
+                if (result != null)
+                {
+                    result.EstadoEntrega = control.EstadoEntrega;
+                    result.HoraEntregada = control.HoraEntregada;
+                    result.UsuarioModificacionLog = control.UsuarioIngresoLog;
+                    result.FechaModificacionLog = DateTime.Now;
+                    result.TerminalModificacionLog = control.TerminalIngresoLog;
+                }
+                else
+                {
+                    control.EstadoRegistro = clsAtributos.EstadoRegistroActivo;
+                    control.FechaIngresoLog = DateTime.Now;
+                    entities.ENTREGA_UNIFORME.Add(control);
+                }
+                entities.SaveChanges();
+                return new RespuestaGeneral { Mensaje = clsAtributos.MsjRegistroGuardado, Respuesta = true };
+            }
+        }
+
+        public RespuestaGeneral EliminarEntregaUniforme(ENTREGA_UNIFORME control)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var result = entities.ENTREGA_UNIFORME.FirstOrDefault(x => x.IdEntregaUniforme == control.IdEntregaUniforme);
+                if (result != null)
+                {
+                    result.EstadoRegistro = clsAtributos.EstadoRegistroInactivo;
+                    result.UsuarioModificacionLog = control.UsuarioIngresoLog;
+                    result.FechaModificacionLog = DateTime.Now;
+                    result.TerminalModificacionLog = control.TerminalIngresoLog;
+                    entities.SaveChanges();
+                }
+                return new RespuestaGeneral { Mensaje = clsAtributos.MsjRegistroGuardado, Respuesta = true };
+            }
+        }
+    }
+}
