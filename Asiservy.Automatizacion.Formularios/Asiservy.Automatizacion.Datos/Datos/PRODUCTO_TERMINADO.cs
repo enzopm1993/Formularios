7f74bbdcca929c47db4cb39d52bcf256e64ad5de
vcrespo <victorrcch@hotmail.com>
2020-02-04 10:39:32 -0500

    modelo

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/PRODUCTO_TERMINADO.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/PRODUCTO_TERMINADO.cs
@@ -17,10 +17,10 @@ namespace Asiservy.Automatizacion.Datos.Datos
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public PRODUCTO_TERMINADO()
         {
+            this.PRODUCTO_TERMINADO_DANIADOS = new HashSet<PRODUCTO_TERMINADO_DANIADOS>();
+            this.PRODUCTO_TERMINADO_DETALLE = new HashSet<PRODUCTO_TERMINADO_DETALLE>();
             this.PRODUCTO_TERMINADO_MATERIALES = new HashSet<PRODUCTO_TERMINADO_MATERIALES>();
             this.PRODUCTO_TERMINADO_TIEMPO_PARADAS = new HashSet<PRODUCTO_TERMINADO_TIEMPO_PARADAS>();
-            this.PRODUCTO_TERMINADO_DETALLE = new HashSet<PRODUCTO_TERMINADO_DETALLE>();
-            this.PRODUCTO_TERMINADO_DANIADOS = new HashSet<PRODUCTO_TERMINADO_DANIADOS>();
         }
     
         public int IdProductoTerminado { get; set; }
@@ -44,14 +44,15 @@ namespace Asiservy.Automatizacion.Datos.Datos
         public Nullable<System.DateTime> FechaModificacionLog { get; set; }
         public string UsuarioModificacionLog { get; set; }
         public string TerminalModificacionLog { get; set; }
+        public Nullable<bool> EstadoReporte { get; set; }
     
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PRODUCTO_TERMINADO_DANIADOS> PRODUCTO_TERMINADO_DANIADOS { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PRODUCTO_TERMINADO_DETALLE> PRODUCTO_TERMINADO_DETALLE { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PRODUCTO_TERMINADO_MATERIALES> PRODUCTO_TERMINADO_MATERIALES { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PRODUCTO_TERMINADO_TIEMPO_PARADAS> PRODUCTO_TERMINADO_TIEMPO_PARADAS { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<PRODUCTO_TERMINADO_DETALLE> PRODUCTO_TERMINADO_DETALLE { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<PRODUCTO_TERMINADO_DANIADOS> PRODUCTO_TERMINADO_DANIADOS { get; set; }
     }
 }
