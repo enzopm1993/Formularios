89b615fcc779c56ef8239e138237a56c6507eba3
vcrespo <victorrcch@hotmail.com>
2020-02-06 16:55:41 -0500

    avance de control de balance

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/ControlBalanza/clsDControlBalanza.cs
@@ -0,0 +1,57 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.ControlBalanza
+{
+    public class clsDControlBalanza
+    {
+        public List<spConsultaControlBalanza> ConsultarControlBalance(DateTime FechaProduccion)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                var listado = db.spConsultaControlBalanza(FechaProduccion).ToList();
+                return listado;
+            }
+        }
+        public void GuardarModificarControlBalanza(CONTROL_BALANZA Control)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                var model = db.CONTROL_BALANZA.FirstOrDefault(x => x.IdControlBalanza == Control.IdControlBalanza || (x.Cedula==Control.Cedula && x.EstadoRegistro==clsAtributos.EstadoRegistroActivo));
+                if(model!= null)
+                {
+                    model.Codigo = Control.Codigo;
+                    model.FechaModificacionLog = DateTime.Now;
+                    model.TerminalModificacionLog = Control.TerminalIngresoLog;
+                    model.UsuarioModificacionLog = Control.UsuarioIngresoLog;
+                }
+                else
+                {
+                    Control.FechaIngresoLog = DateTime.Now;
+                    db.CONTROL_BALANZA.Add(Control);
+                }
+                db.SaveChanges();
+            }
+        }
+
+
+        public void EliminarControl(CONTROL_BALANZA Control)
+        {
+            using (ASIS_PRODEntities db = new ASIS_PRODEntities())
+            {
+                var model = db.CONTROL_BALANZA.FirstOrDefault(x => x.IdControlBalanza == Control.IdControlBalanza);
+                if (model != null)
+                {
+                    model.EstadoRegistro = clsAtributos.EstadoRegistroInactivo;
+                    model.FechaModificacionLog = DateTime.Now;
+                    model.TerminalModificacionLog = Control.TerminalIngresoLog;
+                    model.UsuarioModificacionLog = Control.UsuarioIngresoLog;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/ControlBalanza/ControlBalanza.js
@@ -0,0 +1,147 @@
+﻿
+
+$(document).ready(function () {
+    CargarControlBalanza();
+});
+
+
+function Limpiar() {
+    $("#NombreEmpleado").val('');
+    $("#Identificacion").val('');   
+    $("#txtCodigo").val('');
+}
+
+function CargarControlBalanza() {
+    $("#spinnerCargando").prop("hidden", false);
+    $("#DivTableControl").html('');
+    $.ajax({
+        url: "../ControlBalanza/ControlBalanzaPartial",
+        type: "GET",
+        data: {
+            Fecha: $('#txtFecha').val(),
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            if (resultado == "0") {
+                $("#DivTableControl").html('');
+                $("#DivTableControl").html("No existen registros");
+            } else {
+                $("#DivTableControl").html('');
+                $("#DivTableControl").html(resultado);
+                config.opcionesDT.pageLength = 5;
+                config.opcionesDT.order = [[1, "asc"]];
+                $('#tblDataTable').DataTable(config.opcionesDT);
+
+
+            }           
+            $("#spinnerCargando").prop("hidden", true);
+
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+            $("#DivTableControl").html('');
+            $("#spinnerCargando").prop("hidden", true);
+
+        }
+    });
+}
+
+
+function Validar() {
+    var valida = true;
+    if ($("#txtFecha").val() == "") {
+        $("#txtFecha").css('borderColor', '#FA8072');
+        valida=false;
+    } else {
+        $("#txtFecha").css('borderColor', '#ced4da');
+    } 
+    if ($("#txtCodigo").val() == "") {
+        $("#txtCodigo").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtCodigo").css('borderColor', '#ced4da');
+    } 
+
+    if ($("#NombreEmpleado").val() == "") {
+        $("#NombreEmpleado").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#NombreEmpleado").css('borderColor', '#ced4da');
+    } 
+    return valida;
+}
+
+function GuardarControl() {
+
+    if (!Validar()) {
+        return;
+    }
+
+    $.ajax({
+        url: "../ControlBalanza/ControlBalanza",
+        type: "POST",
+        data: {
+            Fecha: $('#txtFecha').val(),
+            Cedula: $("#Identificacion").val(),
+            Codigo: $("#txtCodigo").val(),
+            Linea: $("#selectLinea").val()
+        },
+        success: function (resultado) {
+            MensajeCorrecto(resultado);
+            CargarControlBalanza();
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+        }
+    });
+}
+
+function EditarControlBalanza(model) {
+    $("#NombreEmpleado").val(model.Nombre);
+    $("#Identificacion").val(model.Cedula);
+    $("#txtCodigo").val(model.Codigo);
+}
+
+
+
+function InactivarControlConsumoDetalle() {
+    $.ajax({
+        url: "../ControlBalanza/EliminarControlBalanza",
+        type: "POST",
+        data: {
+            IdControlBalanza: $("#txtEliminarDetalle").val()
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            if (resultado == "0") {
+                MensajeAdvertencia("Faltan Parametros");
+            }
+            CargarControlBalanza();
+            $("#modalEliminarControl").modal("hide");
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+            $('#btnConsultar').prop("disabled", false);
+            $("#spinnerCargando").prop("hidden", true);
+        }
+    });
+}
+
+function EliminarControlBalanza(model) {
+    $("#txtEliminarDetalle").val(model.IdControlBalanza);
+    //$("#pModalDetalle").html("Hora: " + moment(model.HoraInicio).format('HH:mm') + ' - ' + moment(model.HoraFin).format('HH:mm'));
+    $("#modalEliminarControlDetalle").modal('show');
+}
+
+$("#modal-detalle-si").on("click", function () {
+    InactivarControlConsumoDetalle();
+    $("#modalEliminarControlDetalle").modal('hide');
+});
+
+$("#modal-detalle-no").on("click", function () {
+    $("#modalEliminarControlDetalle").modal('hide');
+});
