126cdd86a6289e583f09b6a112528385502f378c
aintriago <aintriago@asiservy.com>
2020-02-11 11:58:24 -0500

    Personal presente inicial vs actual

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Nomina/ModeloVistaPersonalPresente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Nomina
+{
+    public class ModeloVistaPersonalPresente
+    {
+        public string CodCentroCosto { get; set; }
+        public string CentroCosto { get; set; }
+        public string CodRecurso { get; set; }
+        public string Recurso { get; set; }
+        public string CodCargo { get; set; }
+        public string Cargo { get; set; }
+        public string CodLinea { get; set; }
+        public string Linea { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Nomina/ModeloVistaTablasPersonalPreesente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Nomina
+{
+    public class ModeloVistaTablasPersonalPreesente
+    {
+        public List<ModeloVistaPersonalPresente> ListaAsistenciaInicial { get; set; }
+        public List<ModeloVistaPersonalPresente> ListaAsistenciaActual { get; set; }
+    }
+}
