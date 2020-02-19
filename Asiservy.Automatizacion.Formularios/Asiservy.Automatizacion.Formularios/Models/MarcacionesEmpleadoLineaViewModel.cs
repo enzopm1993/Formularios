fed1c720778c650ec15dc8e9feccc6f3e6a83c25
aintriago <aintriago@asiservy.com>
2020-02-19 00:38:01 -0500

    Registro de marcaciones

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Models/MarcacionesEmpleadoLineaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.Models
+{
+    public class MarcacionesEmpleadoLineaViewModel
+    {
+        public string FECHA_MARCA { get; set; }
+        public string DIA { get; set; }
+        public string CEDULA { get; set; }
+        public string EMPLEADO { get; set; }
+        public string INGRESO { get; set; }
+        public string SALIDA { get; set; }
+    }
+}
