fdbd4512f03b59817084cd497345a9064699b8a6
aintriago <aintriago@asiservy.com>
2020-02-27 08:11:33 -0500

    Agregar Linea, vista de vacaciones

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Vacaciones/VacacionesModelView.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Vacaciones/VacacionesModelView.cs
@@ -7,6 +7,7 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Vacaciones
 {
     public class VacacionesModelView
     {
+        public string Linea { get; set; }
         public string Nombres { get; set; }
         public decimal? TotalDias { get; set; }
         public decimal? DiasTomados { get; set; }
