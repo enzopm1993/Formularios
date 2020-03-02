0b03cbd2081e1bc91369a8c32770bf7687c5b1b9
aintriago <aintriago@asiservy.com>
2020-03-02 13:23:59 -0500

    Opcion Rpt BLZ descongelado emparrillado MP

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/BLZController.cs
@@ -0,0 +1,87 @@
+﻿using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.BLZ;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    [Authorize]
+    public class BLZController : Controller
+    {
+        string[] lsUsuario;
+        clsDError clsDError = null;
+        // GET: BLZ
+        public ActionResult ReporteDescongeladoEmparrilladoMP()
+        {
+            try
+            {
+                ViewBag.dataTableJS = "1";
+                ViewBag.Title = "Reporte de descongelado y emparrillado de Materia Prima";
+                ViewBag.JavaScrip = RouteData.Values["controller"] + "/" + RouteData.Values["action"];
+
+                return View();
+            }
+            catch (DbEntityValidationException e)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+            catch (Exception ex)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GeneraDescongeladoEmparrilladoMP(string fechaPrd)
+        {
+            try
+            {
+
+                lsUsuario = User.Identity.Name.Split('_');
+                string Cedula = lsUsuario[1];
+
+
+                var client = new RestClient(clsAtributos.BASE_URL_WS);
+
+                string URL = "/api/Produccion/ControlDescongeladoEmparrilladoMP/" + fechaPrd;
+
+                var request = new RestRequest(URL, Method.GET);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+                var datos = JsonConvert.DeserializeObject<List<RegistroDescongeladoEmparrilladoMP>>(content);
+
+                JsonResult result = Json(datos, JsonRequestBehavior.AllowGet);
+
+                result.MaxJsonLength = 50000000;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return Json(ex, JsonRequestBehavior.AllowGet);
+            }
+
+
+        }
+
+        protected void SetErrorMessage(string message)
+        {
+            TempData["MensajeError"] = message;
+        }
+    }
+}
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/BLZ/ReporteDescongeladoEmparrilladoMP.js
@@ -0,0 +1,80 @@
+﻿$(function () {
+
+    var iconLoader = "fa-spinner fa-pulse";
+    var iconSearch = "fa-search"
+   
+
+    var table = $("#tblDataTable");
+
+    $("#generarReporte").click(function () {
+        table.DataTable().destroy();
+        table.DataTable().clear();
+        table.DataTable().draw();
+
+        var fechaPrd = $("#fechaPrd").val();
+
+
+        $("#generarReporte").attr('href', "javascript:void(0)");
+        $("#iconSearch").removeClass(iconSearch);
+        $("#iconSearch").addClass(iconLoader);
+        $("#generarReporte").addClass("btnWait");
+        $.ajax({
+            contentType: "application/json; charset=utf-8",
+            url: "../BLZ/GeneraDescongeladoEmparrilladoMP",
+            type: "GET",
+            data: {
+                'fechaPrd': fechaPrd
+            },
+            success: function (resultado) {
+                $("#generarReporte").attr('href', "#");
+                $("#iconSearch").removeClass(iconLoader);
+                $("#iconSearch").addClass(iconSearch);
+                $("#generarReporte").removeClass("btnWait");
+
+                $("#tblDataTable tbody").empty();
+                config.opcionesDT.pageLength = -1;
+                config.opcionesDT.order = [];
+                config.opcionesDT.columns = [
+                    { data: 'U_SYP_LOTE' },
+                    { data: 'ORDEN' },
+                    { data: 'U_SYP_ESPECIE' },
+                    { data: 'U_SYP_TALLA' },
+                    { data: 'U_SYP_BARCO' },
+                    { data: 'U_SYP_CLIENTE' },
+                    { data: 'U_SYP_TANQUE' },
+                    { data: 'VOLTEO' },
+                    { data: 'U_SYP_DUCHA' },
+                    { data: 'U_SYP_TC_AGUA' },
+                    { data: 'HORA_INI_DESC' },
+                    { data: 'U_SYP_TC_INI_DES' },
+                    { data: 'HORA_FIN_DESC' },
+                    { data: 'U_SYP_TC_FIN_DES' },
+                    { data: 'U_SYP_TIEMPO_DES' },
+                    { data: 'HORA_INI_EMPA' },
+                    { data: 'HORA_FIN_EMPA' },
+                    { data: 'U_SYP_RAQUEO_EMPA' },
+                    { data: 'OBSERVACIONES' },
+                    { data: 'U_SYP_USUARIO' }
+                ];
+                table.DataTable().destroy();
+                table.DataTable(config.opcionesDT);
+                //table.DataTable().draw();
+                table.DataTable().clear();
+                table.DataTable().rows.add(resultado);
+                table.DataTable().draw();
+            },
+            error: function (resultado) {
+                console.log(resultado);
+                $("#generarReporte").attr('href', "#");
+                $("#iconSearch").removeClass(iconLoader);
+                $("#iconSearch").addClass(iconSearch);
+                $("#generarReporte").removeClass("btnWait");
+                MensajeError(resultado.statusText, false);
+
+            }
+        });
+
+        return false;
+
+    });
+});
