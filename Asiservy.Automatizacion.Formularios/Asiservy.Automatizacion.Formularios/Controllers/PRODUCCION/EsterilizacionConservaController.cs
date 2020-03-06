567cd82d5212cc56f1b6d7485271927c3f276ab0
prokyo1 <mazza624@gmail.com>
2020-03-06 11:53:10 -0500

    esterilizacion conservas

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/PRODUCCION/EsterilizacionConservaController.cs
@@ -0,0 +1,193 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.General;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.PRODUCCION.CocheAutoclave;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.PRODUCCION.EsterilizacionConserva;
+//using Asiservy.Automatizacion.Formularios.AccesoDatos.PRODUCCION.CocheAutoclave;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers.PRODUCCION
+{
+    public class EsterilizacionConservaController : Controller
+    {
+        string[] lsUsuario = null;
+        clsDError clsDError = null;
+        clsDEsterilizacionConserva clsDEsterilizacionConserva = null;
+        clsDCcocheAutoclave clsDCcocheAutoclave = null;
+        //clsDEmpleado clsDEmpleado = null;
+        //clsDClasificador clsDClasificador = null;
+        //clsDApiProduccion clsDApiProduccion = null;
+        //clsDCcocheAutoclave clsDCcocheAutoclave = null;
+        protected void SetSuccessMessage(string message)
+        {
+            TempData["MensajeConfirmacion"] = message;
+        }
+        protected void SetErrorMessage(string message)
+        {
+            TempData["MensajeError"] = message;
+        }
+        // GET: EsterilizacionConserva
+        [Authorize]
+        public ActionResult ControlEsterilizacionConserva()
+        {
+            try
+            {
+                ViewBag.JavaScrip = "PRODUCCION/" + RouteData.Values["controller"] + "/" + RouteData.Values["action"];
+                ViewBag.dataTableJS = "1";
+                ViewBag.select2 = "1";
+                lsUsuario = User.Identity.Name.Split('_');
+
+                return View();
+            }
+            catch (DbEntityValidationException e)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+            catch (Exception ex)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+        }
+        [HttpPost]
+        public JsonResult GuardarModificarCabeceraEsterilizacion(CABECERA_CONTROL_ESTERILIZACION_CONSERVAS poControlEsterilizacion)
+        {
+            try
+            {
+                
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                poControlEsterilizacion.FechaIngresoLog = DateTime.Now;
+                poControlEsterilizacion.UsuarioIngresoLog = lsUsuario[0];
+                poControlEsterilizacion.TerminalIngresoLog = Request.UserHostAddress;
+                poControlEsterilizacion.EstadoRegistro = clsAtributos.EstadoRegistroActivo;
+                CABECERA_CONTROL_ESTERILIZACION_CONSERVAS resultado=null;
+                clsDEsterilizacionConserva = new clsDEsterilizacionConserva();
+                if (poControlEsterilizacion.IdCabControlEsterilizado == 0)
+                {
+                    resultado = clsDEsterilizacionConserva.GuardarCabEsterilizacionConserva(poControlEsterilizacion);
+                }
+                else
+                {
+                    //update
+                }
+                
+                //clsDControlConsumoInsumo = new clsDControlConsumoInsumo();
+                //string resultado = clsDControlConsumoInsumo.GuardarPallet(pallet);
+                return Json(resultado, JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ConsultarCabeceraEsterilizacion(CABECERA_CONTROL_ESTERILIZACION_CONSERVAS poControlEsterilizacion)
+        {
+            try
+            {
+
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                
+                CABECERA_CONTROL_ESTERILIZACION_CONSERVAS resultado = null;
+                clsDEsterilizacionConserva = new clsDEsterilizacionConserva();
+                resultado = clsDEsterilizacionConserva.ConsultarCabeceraEsterilizacionConserva(poControlEsterilizacion);
+                return Json(new {resultado.IdCabControlEsterilizado,resultado.Observacion,resultado.TipoLinea,resultado.Turno,resultado.Fecha }, JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult PartialCocheAutoclave(DateTime Fecha, string Turno)
+        {
+            try
+            {
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                clsDCcocheAutoclave = new clsDCcocheAutoclave();
+                // clsDEmpleado = new clsDEmpleado();
+                // var Empleado = clsDEmpleado.ConsultaEmpleado(lsUsuario[1]).FirstOrDefault();
+                List<spConsultaCocheAutoclave> model = clsDCcocheAutoclave.ConsultaCocheAutoclave(Fecha, Turno);
+                if (!model.Any())
+                {
+                    return Json("0", JsonRequestBehavior.AllowGet);
+                }
+                return PartialView(model);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
