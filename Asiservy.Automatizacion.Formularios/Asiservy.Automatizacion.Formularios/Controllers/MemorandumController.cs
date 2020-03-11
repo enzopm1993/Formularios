e4ccae3fb42e2c963eb0d67b09f126f55889f1f5
Gabriel Intriago <agia_1991@hotmail.com>
2020-03-10 23:32:17 -0500

    Memos

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/MemorandumController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers
+{
+    public class MemorandumController : Controller
+    {
+        // GET: Memorandum
+        public ActionResult Gestion()
+        {
+            ViewBag.Title = "Gestión de memorandum";
+            ViewBag.JavaScrip = RouteData.Values["controller"] + "/" + RouteData.Values["action"];
+
+
+
+            return View();
+        }
+
+        public ActionResult Generar()
+        {
+            return View();
+        }
+
+    }
+}
