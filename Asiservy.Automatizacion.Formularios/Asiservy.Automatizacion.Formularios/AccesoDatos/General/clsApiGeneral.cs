c22731292bdcaae5fec0fe11871840dea39c3248
vcrespo <victorrcch@hotmail.com>
2020-03-23 12:32:09 -0500

    Comunicados en el home

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/General/clsApiGeneral.cs
@@ -0,0 +1,27 @@
+﻿using Asiservy.Automatizacion.Formularios.AccesoDatos.App;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.General
+{
+    public class clsApiGeneral
+    {
+
+        public List<Comunicados> ConsultaComunicados()
+        {
+            var client = new RestClient("http://192.168.0.31:8870");
+            var request = new RestRequest("/api/Comunicados/App", Method.GET);
+            IRestResponse response = client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.InternalServerError)
+                return new List<Comunicados>();
+            var content = response.Content;
+            var ListaUsuarios = JsonConvert.DeserializeObject<List<Comunicados>>(content);
+            return (List<Comunicados>)ListaUsuarios;
+        }
+    }
+}
