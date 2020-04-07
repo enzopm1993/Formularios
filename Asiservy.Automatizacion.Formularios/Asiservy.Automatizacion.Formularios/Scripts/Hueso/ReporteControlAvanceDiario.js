45328a16447c96a8f8978d204a0be314c61f31a3
vcrespo <victorrcch@hotmail.com>
2020-04-06 20:03:24 -0500

    avance de reporte de control de hueso

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Hueso/ReporteControlAvanceDiario.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/Hueso/ReporteControlAvanceDiario.js
@@ -1,25 +1,46 @@
 ﻿
 function CargarReporteAvance() {
-    var txtFecha = $('#txtFecha').val();
+    //var txtFecha = $('#txtFecha').val();
+    var txtFechaDesde = $('#txtFechaDesde').val();
+    var txtFechaHasta = $('#txtFechaHasta').val();
     var selectLinea = $('#selectLinea').val();
-   // console.log(selectLinea);
-    if (txtFecha == "") {
-        MensajeAdvertencia("Igrese una Fecha");
+    var bitacora = $('#DivTableReporteControlAvance');
+    bitacora.html('');
+
+    // console.log(selectLinea);
+
+   
+    if (txtFechaDesde == "" || txtFechaHasta == "") {
+        MensajeAdvertencia("Igrese un rango de fechas");
         return;
     }
+    if (txtFechaDesde > txtFechaHasta) {
+        MensajeAdvertencia("Fecha hasta no puede ser menor a fecha desde");
+        return;
+    }
+    if (moment(txtFechaDesde).format("MMMM") != moment(txtFechaHasta).format("MMMM")) {
+        MensajeAdvertencia("Solo puede consultar un mes");
+        return;
+    }
+   // console.log(moment(txtFechaDesde).format("MMMM"));
+
     if (selectLinea == "") {
         MensajeAdvertencia("Seleccione una Linea");
         return;
     }
+    if ($("#selectTurno").val() == "") {
+        MensajeAdvertencia("Seleccione un turno");
+        return;
+    }
     $('#btnConsultar').prop("disabled", true);
     $("#spinnerCargando").prop("hidden", false);
-    var bitacora = $('#DivTableReporteControlAvance');
-    bitacora.html('');
     $.ajax({
         url: "../Hueso/ReporteControlAvanceDiarioPartial",
         type: "GET",
         data: {
-            ddFecha:txtFecha,
+            ddFechaDesde: txtFechaDesde,
+            ddFechaHasta: txtFechaHasta,
+            Turno: $("#selectTurno").val(),
             dsLinea: selectLinea
         },
         success: function (resultado) {
