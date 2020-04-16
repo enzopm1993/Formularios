c712377562c03accae2f11730d10fbf2304291f4
vcrespo <victorrcch@hotmail.com>
2020-04-15 20:35:26 -0500

    avance de REPORTES MAESTRO

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/Reporte/clsDReporte.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.Reporte
+{
+    public class clsDReporte
+    {
+    }
+}
