5f8b76c79b42bec6bc2675ea4ade9aa5b3f93f04
vcrespo <victorrcch@hotmail.com>
2020-04-23 17:47:57 -0500

    avance de Maestro de reporte

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/ReporteMaestro/ReporteMaestro.js
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/ReporteMaestro/ReporteMaestro.js
@@ -88,12 +88,186 @@ function GenerarReporteMaestro() {
     });
 }
 
+function NuevoControl() {
+    $("#txtIdControl").val('0');
+    $("#txtNombre").val('');
+    $("#txtCodigo").val('');
+    $("#txtVersion").val('');
+
+    $("#txtIdControlModal").val('0');
+    $("#txtNombreModal").val('');
+    $("#txtCodigoModal").val('');
+    $("#txtVersionModal").val('');
+
+}
 
 function SeleccionarReporteMaestro(model) {
-    alert(model);
+   // alert(model);
+    $("#hMensaje").prop("hidden", false);
+    $("#hMensaje").html(model.Nombre);
 
     $("#txtIdControl").val(model.IdReporteMaestro);
     $("#txtNombre").val(model.Nombre);
     $("#txtCodigo").val(model.Codigo);
     $("#txtVersion").val(model.UltimaVersion);
+
+    $("#txtIdControlModal").val(model.IdReporteMaestro);
+    $("#txtNombreModal").val(model.Nombre);
+    $("#txtCodigoModal").val(model.Codigo);
+    $("#txtVersionModal").val(model.UltimaVersion);
+
+    $("#divCabecera").prop("hidden", true);
+    $("#btnGenerar").prop("hidden", true);
+    $("#divCabeceraPartial").prop("hidden", true);
+
+    $("#btnEditar").prop("hidden", false);
+    $("#btnAtras").prop("hidden", false);
+    $("#divDetalle").prop("hidden", false);
+
+
+    ConsultarReporteDetalle(model.IdReporteMaestro);
+
+}
+function Atras() {
+
+    $("#hMensaje").prop("hidden", true);
+    $("#hMensaje").html('');
+
+    $("#divCabecera").prop("hidden", false);
+    $("#btnGenerar").prop("hidden", false);
+    $("#divCabeceraPartial").prop("hidden", false);
+
+    $("#btnEditar").prop("hidden", true);
+    $("#btnAtras").prop("hidden", true);
+    $("#divDetalle").prop("hidden", true);
+    NuevoControl();
+    ConsultarReporteMaestros();
+}
+
+
+function EditarReporte() {
+    $("#ModalEditarControl").modal("show");
+}
+
+function EditarReporteMaestro() {
+    if ($("#txtIdControlModal").val() == "") {
+        $("#txtIdControlModal").css('borderColor', '#FA8072');
+        return;
+    } else {
+        $("#txtIdControlModal").css('borderColor', '#ced4da');
+    }
+    if ($("#txtNombreModal").val() == "") {
+        $("#txtNombreModal").css('borderColor', '#FA8072');
+        return;
+    } else {
+        $("#txtNombreModal").css('borderColor', '#ced4da');
+    }
+    if ($("#txtCodigoModal").val() == "") {
+        $("#txtCodigoModal").css('borderColor', '#FA8072');
+        return;
+    } else {
+        $("#txtCodigoModal").css('borderColor', '#ced4da');
+    }
+    if ($("#txtVersionModal").val() == "") {
+        $("#txtVersionModal").css('borderColor', '#FA8072');
+        return;
+    } else {
+        $("#txtVersionModal").css('borderColor', '#ced4da');
+    }
+    $.ajax({
+        url: "../ReporteMaestro/ReporteMaestro",
+        type: "POST",
+        data: {
+            IdReporteMaestro: $("#txtIdControlModal").val(),
+            Nombre: $("#txtNombreModal").val(),
+            Codigo: $("#txtCodigoModal").val(),
+            UltimaVersion: $("#txtVersionModal").val()
+
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            } else if (resultado == "0") {
+                MensajeAdvertencia("Faltan parametros");
+                return;
+            }
+            //      NuevaReporteMaestro();
+            $("#ModalEditarControl").modal("hide");
+            MensajeCorrecto("Registro Exitoso");
+            //ConsultarReporteMaestros();
+        },
+        error: function (result) {
+           /* console.log(JSON.stringify(result.responseText))*/;
+            MensajeError("Error, comuniquese con sistemas. " + result, false);
+        }
+    });
+}
+
+
+
+//////////////////////////////////////DETALLE///////////////////////////////////////////////
+
+function ConsultarReporteDetalle(Id) {
+
+    $("#spinnerCargandoDetalle").prop("hidden", false);
+    $.ajax({
+        url: "../ReporteMaestro/ReporteDetallePartial",
+        type: "GET",
+        data: {
+            idControl: Id
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            if (resultado == "0") {
+                $("#chartDetalle").html("No existen registros");
+                $("#spinnerCargandoDetalle").prop("hidden", true);
+            } else {
+                $("#spinnerCargandoDetalle").prop("hidden", true);
+                $("#chartDetalle").html(resultado);
+                config.opcionesDT.pageLength = -1;
+                config.opcionesDT.order = [];
+                $('#tblDataTable2').DataTable(config.opcionesDT);
+            }
+            //  $('#btnConsultar').prop("disabled", true);
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+            $("#spinnerCargando").prop("hidden", true);
+        }
+    });
+}
+
+
+function GenerarReporteDetalle() {
+    $("#ModalControlDetalle").modal("show");
+}
+
+
+
+function readFile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var filePreview = document.createElement('img');
+            filePreview.id = 'file-preview';
+            //e.target.result contents the base64 data from the image uploaded
+            filePreview.src = e.target.result;
+            console.log(e.target.result);
+
+            var previewZone = document.getElementById('file-preview-zone');
+            previewZone.appendChild(filePreview);
+            document.getElementById("file-preview").style.width = "200px";
+            document.getElementById("file-preview").style.height = "300px";
+}
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+var fileUpload = document.getElementById('file-upload');
+fileUpload.onchange = function (e) {
+    readFile(e.srcElement);
 }
