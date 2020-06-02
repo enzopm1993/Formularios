ea0fba6d84c0658582ac5be2849ad9ed6507e259
vcrespo <victorrcch@hotmail.com>
2020-06-02 13:43:31 -0500

    ajustes de eliminar controles Control de analiss de agua de tanquero

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/CALIDAD/AnalisisAguaTanqueroController.cs
@@ -0,0 +1,240 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using Asiservy.Automatizacion.Formularios.AccesoDatos;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.CALIDAD.AnalisisAguaTanquero;
+using Asiservy.Automatizacion.Formularios.AccesoDatos.Reporte;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asiservy.Automatizacion.Formularios.Controllers.CALIDAD
+{
+    public class AnalisisAguaTanqueroController : Controller
+    {
+
+        private ClsdAnalisisAguaTanquero ClsdAnalisisAguaTanquero { get; set; } = null;
+        private clsDError clsDError { get; set; } = null;
+        private clsDReporte clsDReporte { get; set; } = null;
+        private string[] lsUsuario { get; set; } = null;
+
+        // GET: AnalisisAguaTanquero
+        #region CONTROL 
+        [Authorize]
+        public ActionResult AnalisisAguaTanquero()
+        {
+            try
+            {
+                ViewBag.JavaScrip = "CALIDAD/" + RouteData.Values["controller"] + "/" + RouteData.Values["action"];
+                ViewBag.dataTableJS = "1";
+                ViewBag.select2 = "1";
+                ViewBag.MaskedInput = "1";
+                return View();
+            }
+            catch (DbEntityValidationException e)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+            catch (Exception ex)
+            {
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                SetErrorMessage(Mensaje);
+                return RedirectToAction("Home", "Home");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult AnalisisAguaTanquero(CC_ANALISIS_AGUA_TANQUERO model, DateTime Fecha)
+        {
+            try
+            {
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+
+                ClsdAnalisisAguaTanquero = new ClsdAnalisisAguaTanquero();
+                model.EstadoRegistro = clsAtributos.EstadoRegistroActivo;
+                model.FechaIngresoLog = DateTime.Now;
+                model.UsuarioIngresoLog = lsUsuario[0];
+                model.TerminalIngresoLog = Request.UserHostAddress;
+                if (ClsdAnalisisAguaTanquero.ConsultaAnalisisAguaTanqueroControl(Fecha).Any(x => x.EstadoReporte))
+                {
+                    return Json(1, JsonRequestBehavior.AllowGet);
+                }
+                ClsdAnalisisAguaTanquero.GuardarModificarAnalisisAguaTanquero(model, Fecha);
+
+                return Json("Registro Exitoso", JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult AnalisisAguaTanqueroPartial(DateTime Fecha)
+        {
+            try
+            {
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                ClsdAnalisisAguaTanquero = new ClsdAnalisisAguaTanquero();
+                var model = ClsdAnalisisAguaTanquero.ConsultaAnalisisAguaTanquero(Fecha);
+                if (!model.Any())
+                {
+                    return Json("0", JsonRequestBehavior.AllowGet);
+                }
+                return PartialView(model);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult EliminarAnalisisAguaTanquero(CC_ANALISIS_AGUA_TANQUERO model, DateTime Fecha)
+        {
+            try
+            {
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                if (model.IdAnalisisAguaTanquero == 0)
+                {
+                    return Json("0", JsonRequestBehavior.AllowGet);
+                }
+                model.FechaIngresoLog = DateTime.Now;
+                model.TerminalIngresoLog = Request.UserHostAddress;
+                model.UsuarioIngresoLog = lsUsuario[0];
+                model.EstadoRegistro = clsAtributos.EstadoRegistroInactivo;
+                ClsdAnalisisAguaTanquero = new ClsdAnalisisAguaTanquero();
+                if (ClsdAnalisisAguaTanquero.ConsultaAnalisisAguaTanqueroControl(Fecha).Any(x => x.EstadoReporte))
+                {
+                    return Json(1, JsonRequestBehavior.AllowGet);
+                }
+                ClsdAnalisisAguaTanquero.EliminarAnalisisAguaTanquero(model);
+                return Json("Registro Eliminado", JsonRequestBehavior.AllowGet);
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult ValidaEstadoReporte(DateTime Fecha)
+        {
+            try
+            {
+                lsUsuario = User.Identity.Name.Split('_');
+                if (string.IsNullOrEmpty(lsUsuario[0]))
+                {
+                    return Json("101", JsonRequestBehavior.AllowGet);
+                }
+                ClsdAnalisisAguaTanquero = new ClsdAnalisisAguaTanquero();
+                var control = ClsdAnalisisAguaTanquero.ConsultaAnalisisAguaTanqueroControl(Fecha).FirstOrDefault();
+                if (control != null)
+                {
+                    if (control.EstadoReporte)
+                    {
+                        return Json(1, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        return Json(2, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                else
+                {
+                    return Json(0, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), null, e);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                clsDError = new clsDError();
+                lsUsuario = User.Identity.Name.Split('_');
+                string Mensaje = clsDError.ControlError(lsUsuario[0], Request.UserHostAddress, this.ControllerContext.RouteData.Values["controller"].ToString(),
+                    "Metodo: " + this.ControllerContext.RouteData.Values["action"].ToString(), ex, null);
+                return Json(Mensaje, JsonRequestBehavior.AllowGet);
+            }
+        }
+        #endregion
+
+
+        protected void SetSuccessMessage(string message)
+        {
+            TempData["MensajeConfirmacion"] = message;
+        }
+        protected void SetErrorMessage(string message)
+        {
+            TempData["MensajeError"] = message;
+        }
+    }
+}
---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Scripts/CALIDAD/AnalisisAguaTanquero/AnalisisAguaTanquero.js
@@ -0,0 +1,276 @@
+﻿$(document).ready(function () {
+    ConsultarControl();
+    $("#txtPh").mask("9?.99");
+});
+
+function ValidaEstadoReporte(Fecha) {
+    $.ajax({
+        url: "../AnalisisAguaTanquero/ValidaEstadoReporte",
+        type: "GET",
+        data: {
+            Fecha: Fecha
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            if (resultado == 0) {
+                $("#lblAprobadoPendiente").html("");
+
+            } else if (resultado == 1) {
+                $("#lblAprobadoPendiente").removeClass("badge-danger").addClass("badge-info");
+                $("#lblAprobadoPendiente").html(Mensajes.Aprobado);
+
+            } else {
+                $("#lblAprobadoPendiente").removeClass("badge-info").addClass("badge-danger");
+                $("#lblAprobadoPendiente").html(Mensajes.Pendiente);
+            }
+        },
+        error: function (resultado) {
+            MensajeError("Error: Comuníquese con sistemas", false);
+        }
+    });
+}
+
+function ConsultarControl() {
+    $("#chartCabecera2").html('');
+    if ($("#txtFecha").val() == '') {
+        return;
+    }
+    $("#spinnerCargando").prop("hidden", false);
+    ValidaEstadoReporte($("#txtFecha").val());
+    $.ajax({
+        url: "../AnalisisAguaTanquero/AnalisisAguaTanqueroPartial",
+        type: "GET",
+        data: {
+            Fecha: $("#txtFecha").val()
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            $("#divCabecera2").prop("hidden", false);
+            if (resultado == "0") {
+                $("#chartCabecera2").html('<div class="text-center"><h4 class="text-warning">' + Mensajes.SinRegistros + '</h4></div>');
+                $("#spinnerCargando").prop("hidden", true);
+            } else {
+                $("#spinnerCargando").prop("hidden", true);
+                $("#chartCabecera2").html(resultado);
+                config.opcionesDT.pageLength = -1;
+                config.opcionesDT.order = false;
+                config.opcionesDT.ordering = false;
+                $('#tblDataTable').DataTable(config.opcionesDT);
+            }
+            //  $('#btnConsultar').prop("disabled", true);
+        },
+        error: function (resultado) {
+            console.log(resultado);
+            MensajeError("Error: Comuníquese con sistemas", false);
+            $('#btnConsultar').prop("disabled", false);
+            $("#spinnerCargando").prop("hidden", true);
+        }
+    });
+}
+
+
+function SeleccionarControl(model) {
+   // console.log(model);
+    $("#txtFecha").prop("disabled", true);
+    $("#txtIdAnalisisAguaTanquero").val(model.IdAnalisisAguaTanquero);
+    $("#txtHora").val(moment(model.Hora).format("YYYY-MM-DDTHH:mm"));
+    $("#txtObservacion").val(model.Observacion);
+    $("#txtPlaca").val(model.Placa);
+    $("#txtStd").val(model.Std);
+    $("#txtDureza").val(model.Dureza);
+    $("#txtPh").val(model.Ph);
+    $("#txtDestino").val(model.Destino);
+    $("#chkOlor").prop("checked", model.Olor);
+    $("#chkColor").prop("checked", model.Color);
+    $("#chkSabor").prop("checked", model.Sabor);
+    $("#btnEliminar").prop("hidden", false);
+}
+
+function NuevoControl() {
+    $("#txtFecha").prop("disabled", false);
+    $("#txtIdAnalisisAguaTanquero").val('0');
+    $("#txtHora").val(moment().format("YYYY-MM-DDTHH:mm"));
+    $("#txtObservacion").val('');
+    $("#txtPlaca").val('');
+    $("#txtStd").val('');
+    $("#txtDureza").val('');
+    $("#txtPh").val('');
+    $("#txtDestino").val('');
+    $("#chkOlor").prop("checked", false);
+    $("#chkColor").prop("checked", false);
+    $("#chkSabor").prop("checked", false);
+
+    $("#btnEliminar").prop("hidden", true);
+
+}
+
+function Validar() {
+    var valida = true;
+
+    if ($("#txtHora").val() == "") {
+        $("#txtHora").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtHora").css('borderColor', '#ced4da');
+    }
+
+    if ($("#txtFecha").val() == "") {
+        $("#txtFecha").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtFecha").css('borderColor', '#ced4da');
+    }
+
+
+    if ($("#txtPlaca").val() == "") {
+        $("#txtPlaca").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtPlaca").css('borderColor', '#ced4da');
+    }
+
+    if ($("#txtStd").val() == "") {
+        $("#txtStd").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtStd").css('borderColor', '#ced4da');
+    }
+
+    if ($("#txtDureza").val() == "") {
+        $("#txtDureza").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtDureza").css('borderColor', '#ced4da');
+    }
+
+    if ($("#txtPh").val() == "") {
+        $("#txtPh").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtPh").css('borderColor', '#ced4da');
+    }
+
+    if ($("#txtDestino").val() == "") {
+        $("#txtDestino").css('borderColor', '#FA8072');
+        valida = false;
+    } else {
+        $("#txtDestino").css('borderColor', '#ced4da');
+    }
+
+    return valida;
+}
+
+
+function GuardarControl() {
+    if (!Validar()) {
+        return;
+    }
+    if (moment($("#txtFecha").val()).format("YYYY-MM-DD") > moment().format("YYYY-MM-DD")) {
+        $("#txtFecha").val("");
+        MensajeAdvertencia("Fecha no permitida");
+        return;
+    }
+    MostrarModalCargando();
+    $.ajax({
+        url: "../AnalisisAguaTanquero/AnalisisAguaTanquero",
+        type: "POST",
+        data: {
+            IdAnalisisAguaTanquero: $("#txtIdAnalisisAguaTanquero").val(),
+            Fecha: $("#txtFecha").val(),
+            Hora: $("#txtHora").val(),
+            Placa: $("#txtPlaca").val(),
+            Std: $("#txtStd").val(),
+            Dureza: $("#txtDureza").val(),
+            Ph: $("#txtPh").val(),
+            Olor: $("#chkOlor").prop("checked"), 
+            Color: $("#chkColor").prop("checked"), 
+            Sabor: $("#chkSabor").prop("checked"), 
+            Destino: $("#txtDestino").val(), 
+            Observacion: $("#txtObservacion").val()
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            CerrarModalCargando();
+
+            if (resultado == "0") {
+                MensajeAdvertencia("Faltan Parametros");
+                return;
+            }
+            if (resultado == "1") {
+                $("#lblAprobadoPendiente").removeClass("badge-danger").addClass("badge-info");
+                $("#lblAprobadoPendiente").html(Mensajes.Aprobado);
+                MensajeAdvertencia(Mensajes.ControlAprobado);
+            } else {
+                NuevoControl();
+                ConsultarControl();
+            }
+            //  $('#btnConsultar').prop("disabled", true);
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+            CerrarModalCargando();
+        }
+    });
+
+    //alert("generado");
+}
+
+
+
+function InactivarControl() {
+    $.ajax({
+        url: "../AnalisisAguaTanquero/EliminarAnalisisAguaTanquero",
+        type: "POST",
+        data: {
+            IdAnalisisAguaTanquero: $("#txtIdAnalisisAguaTanquero").val(),
+            Fecha: $("#txtFecha").val()
+        },
+        success: function (resultado) {
+            if (resultado == "101") {
+                window.location.reload();
+            }
+            if (resultado == "0") {
+                MensajeAdvertencia("Faltan Parametros");
+            }
+            if (resultado == "1") {
+                $("#lblAprobadoPendiente").removeClass("badge-danger").addClass("badge-info");
+                $("#lblAprobadoPendiente").html(Mensajes.Aprobado);
+                MensajeAdvertencia(Mensajes.ControlAprobado);
+            } else {
+                ConsultarControl();
+                NuevoControl();
+            }
+            $("#modalEliminarControl").modal("hide");
+        },
+        error: function (resultado) {
+            MensajeError(resultado.responseText, false);
+            $('#btnConsultar').prop("disabled", false);
+            $("#spinnerCargando").prop("hidden", true);
+        }
+    });
+}
+
+function EliminarControl() {
+    //  $("#txtEliminarDetalle").val($("#txtIdAnalisisAguaTanquero").val());
+    //$("#pModalDetalle").html("Hora: " + moment(model.HoraInicio).format('HH:mm') + ' - ' + moment(model.HoraFin).format('HH:mm'));
+    if ($("#txtIdAnalisisAguaTanquero").val() > 0) {
+        $("#modalEliminarControlDetalle").modal('show');
+    } else {
+        MensajeAdvertencia("Seleccione un control.");
+    }
+}
+
+$("#modal-detalle-si").on("click", function () {
+    InactivarControl();
+    $("#modalEliminarControlDetalle").modal('hide');
+});
+
+$("#modal-detalle-no").on("click", function () {
+    $("#modalEliminarControlDetalle").modal('hide');
+});
