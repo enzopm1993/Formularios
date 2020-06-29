9f486019bc197502c87a21965e0e30aeb99b7066
Ing Victor <victorrcch@hotmail.com>
2020-06-29 17:09:43 -0500

    avance de kpi consumo enlatado pouch

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/PRODUCCION/KpiProduccion/CldsKpiProduccion.cs
@@ -0,0 +1,20 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.PRODUCCION.KpiEnvaseLata
+{
+    public class ClsdKpiProduccion
+    {
+        public List<spConsultaKpiEnvaseLata> ConsultaKpiEnvaseLatas(DateTime FechaDesde, DateTime FechaHasta, string Turno, String Linea)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var lista = entities.spConsultaKpiEnvaseLata(FechaDesde, FechaHasta, Turno,Linea).ToList();
+                return lista;
+            }
+        }
+
+    }
+}
