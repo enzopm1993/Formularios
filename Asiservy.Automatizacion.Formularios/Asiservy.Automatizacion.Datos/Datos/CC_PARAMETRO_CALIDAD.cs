3f2fe5052bb2bc5797bc323a7a5d437e0cc331be
Ing Victor <victorrcch@hotmail.com>
2020-06-30 11:11:29 -0500

    modelo

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/CC_PARAMETRO_CALIDAD.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Datos/Datos/CC_PARAMETRO_CALIDAD.cs
@@ -12,29 +12,18 @@ namespace Asiservy.Automatizacion.Datos.Datos
     using System;
     using System.Collections.Generic;
     
-    public partial class ENTRADA_SALIDA_MATERIAL_CABECERA
+    public partial class CC_PARAMETRO_CALIDAD
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public ENTRADA_SALIDA_MATERIAL_CABECERA()
-        {
-            this.ENTRADA_SALIDA_MATERIAL_DETALLE = new HashSet<ENTRADA_SALIDA_MATERIAL_DETALLE>();
-        }
-    
-        public int IdControlEntradaSalidaMateriales { get; set; }
-        public System.DateTime Fecha { get; set; }
-        public string Linea { get; set; }
-        public string Turno { get; set; }
+        public int IdParametro { get; set; }
+        public string Nombre { get; set; }
+        public Nullable<decimal> Maximo { get; set; }
+        public Nullable<decimal> Minimo { get; set; }
         public string Observacion { get; set; }
-        public string EstadoRegistro { get; set; }
         public System.DateTime FechaIngresoLog { get; set; }
         public string UsuarioIngresoLog { get; set; }
         public string TerminalIngresoLog { get; set; }
         public Nullable<System.DateTime> FechaModificacionLog { get; set; }
         public string UsuarioModificacionLog { get; set; }
         public string TerminalModificacionLog { get; set; }
-        public bool EstadoControl { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<ENTRADA_SALIDA_MATERIAL_DETALLE> ENTRADA_SALIDA_MATERIAL_DETALLE { get; set; }
     }
 }
