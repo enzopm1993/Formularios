d5bc4fcacfd5efb1b90fc7852b122115b66f46ef
Ing Victor <victorrcch@hotmail.com>
2020-07-12 11:04:08 -0500

    oka

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/ControlHueso/clsDControlHueso.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/ControlHueso/clsDControlHueso.cs
@@ -244,7 +244,7 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.ControlHueso
                 }
                 if (ListadoControlAvanceApi.Any())
                 {
-                    var prueba = ListadoControlAvanceApi.Distinct().ToList();
+                    //var prueba = ListadoControlAvanceApi.Distinct().ToList();
                     entities.CONTROL_AVANCE_API.AddRange(ListadoControlAvanceApi.Distinct());                   
                 }
                 entities.SaveChanges();
