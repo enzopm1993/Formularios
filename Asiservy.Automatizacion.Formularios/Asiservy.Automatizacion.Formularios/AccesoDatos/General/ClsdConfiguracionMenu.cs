6daafd5cb7d9a88b66751c482a0036968da7e112
Ing Victor <victorrcch@hotmail.com>
2020-07-15 15:46:21 -0500

    configuracion de menu fondos del login

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/General/ClsdConfiguracionMenu.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/General/ClsdConfiguracionMenu.cs
@@ -17,16 +17,32 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.General
             }
         }
 
+        public FONDO_LOGIN ConsultaLoginFondoActivo()
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                return entities.FONDO_LOGIN.AsNoTracking().FirstOrDefault(x=> x.Vigente);
+            }
+        }
+
 
         public void GuardarModificarLoginFondo(FONDO_LOGIN model)
         {
             using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
             {
+                if (model.Vigente)
+                {
+                    var fondos = entities.FONDO_LOGIN.ToList();
+                    foreach(var f in fondos)
+                    {
+                        f.Vigente = false;
+                    }
+                }       
                 var poControl = entities.FONDO_LOGIN.FirstOrDefault(x => x.IdFondoLogin == model.IdFondoLogin);
                 if (poControl != null)
                 {
                     poControl.Descripcion = model.Descripcion;
-                    poControl.Imagen = model.Imagen;
+                    if (model.Imagen != null) { poControl.Imagen = model.Imagen; }
                     poControl.TerminalModificacionLog = model.TerminalIngresoLog;
                     poControl.UsuarioModificacionLog = model.UsuarioIngresoLog;
                     poControl.FechaModificacionLog = model.FechaIngresoLog;
@@ -35,6 +51,30 @@ namespace Asiservy.Automatizacion.Formularios.AccesoDatos.General
                 {
                     entities.FONDO_LOGIN.Add(model);
                 }
+                entities.SaveChanges();
+            }
+        }
+
+        public void ActivarLoginFondo(FONDO_LOGIN model)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+              
+                var fondos = entities.FONDO_LOGIN.ToList();
+                foreach (var f in fondos)
+                {
+                    f.Vigente = false;
+                }
+               
+                var poControl = entities.FONDO_LOGIN.FirstOrDefault(x => x.IdFondoLogin == model.IdFondoLogin);
+                if (poControl != null)
+                {
+                    poControl.Vigente = true;
+                    poControl.TerminalModificacionLog = model.TerminalIngresoLog;
+                    poControl.UsuarioModificacionLog = model.UsuarioIngresoLog;
+                    poControl.FechaModificacionLog = model.FechaIngresoLog;
+                }
+               
                 entities.SaveChanges();
             }
         }
