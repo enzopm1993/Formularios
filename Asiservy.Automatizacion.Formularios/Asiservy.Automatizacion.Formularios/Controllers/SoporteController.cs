c96764ff0e76c0b8d7c83966f8841951e831bf10
Gabriel Intriago <agia_1991@hotmail.com>
2020-07-19 13:01:31 -0500

    Rpt Tickets

--- Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/SoporteController.cs
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/Controllers/SoporteController.cs
@@ -23,24 +23,18 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
             return View();
         }
         [HttpGet]
-        public JsonResult ObtenerMarcacionesDia(string fechaIni, string fechaFin)
+        public JsonResult ObtenerSoportesReporte(string fechaIni, string fechaFin)
         {
             try
             {
                 var client = new RestClient("http://192.168.0.4/");
 
-                string URL = "/osticket/scp/report.php?fechaini="+ fechaIni + "&fechafin="+ fechaFin;
+                string URL = "/osticket/scp/report.php?tipo=todos&fechaini=" + fechaIni + "&fechafin="+ fechaFin;
                 var request = new RestRequest(URL, Method.GET);
                 IRestResponse response = client.Execute(request);
                 var content = response.Content;
                 List<ItemTicket> dataView = JsonConvert.DeserializeObject<List<ItemTicket>>(content);
-
-                string URLHOY = "/osticket/scp/report.php?fechaini=" + DateTime.Now.ToString("yyyy-MM-dd") + "&fechafin=" + DateTime.Now.ToString("yyyy-MM-dd");
-                var requestHOY = new RestRequest(URLHOY, Method.GET);
-                IRestResponse responseHOY = client.Execute(requestHOY);
-                var contentHOY = responseHOY.Content;
-                List<ItemTicket> TicketsHoy = JsonConvert.DeserializeObject<List<ItemTicket>>(contentHOY);
-
+                
                 ReturnVista envioVista = new ReturnVista();
 
 
@@ -50,6 +44,15 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
                 tkTotales.Cerrados = dataView.Where(c => c.Estado == "Cerrado").Count();
                 tkTotales.SinAsignar = dataView.Where(c => string.IsNullOrEmpty(c.AgenteAsignado)).Count();
 
+
+                var DepColores = dataView
+                    .GroupBy(x => new { x.Departamento, x.Color })
+                    .Select(g => new ClsKeyValue
+                    {
+                        Codigo = g.Key.Departamento,
+                        Descripcion = g.Key.Color
+
+                    }).ToList();
 
                 var resultTicketsPorDep = dataView
                     .GroupBy(x => x.Departamento)
@@ -63,6 +66,7 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
                 envioVista.DataPlana = dataView;
                 envioVista.Totales = tkTotales;
                 envioVista.TotalTicketsPorDep = resultTicketsPorDep;
+                envioVista.DepartamentoColores = DepColores;
                 JsonResult result = Json(envioVista, JsonRequestBehavior.AllowGet);
 
                 result.MaxJsonLength = 50000000;
@@ -73,7 +77,57 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
                 return Json(ex, JsonRequestBehavior.AllowGet);
             }
         }
+        [HttpGet]
+        public JsonResult ObtenerSoportesReporteCerrados(string fechaIni, string fechaFin)
+        {
+            try
+            {
+                var client = new RestClient("http://192.168.0.4/");
 
+                string URL = "/osticket/scp/report.php?tipo=cerrados&fechaini=" + fechaIni + "&fechafin=" + fechaFin;
+                var request = new RestRequest(URL, Method.GET);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+                List<ItemTicket> dataView = JsonConvert.DeserializeObject<List<ItemTicket>>(content);
+
+                ReturnVista envioVista = new ReturnVista();
+
+
+                TkTotales tkTotales = new TkTotales();
+                tkTotales.Totales = dataView.Count();
+
+                var DepColores = dataView
+                    .GroupBy(x => new { x.Departamento, x.Color })
+                    .Select(g => new ClsKeyValue
+                    {
+                        Codigo = g.Key.Departamento,
+                        Descripcion = g.Key.Color
+
+                    }).ToList();
+
+                var resultTicketsPorDep = dataView
+                    .GroupBy(x => x.Departamento)
+                    .Select(g => new ClsKpiDescripcionTotal
+                    {
+                        Descripcion = g.Key,
+                        Total = g.Count()
+
+                    }).OrderByDescending(c => c.Total).ToList();
+
+                envioVista.DataPlana = dataView;
+                envioVista.Totales = tkTotales;
+                envioVista.TotalTicketsPorDep = resultTicketsPorDep;
+                envioVista.DepartamentoColores = DepColores;
+                JsonResult result = Json(envioVista, JsonRequestBehavior.AllowGet);
+
+                result.MaxJsonLength = 50000000;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return Json(ex, JsonRequestBehavior.AllowGet);
+            }
+        }
 
     }
 
@@ -86,6 +140,7 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
         public List<ItemTicket> DataPlana { get; set; }
         public TkTotales Totales { get; set; }
         public List<ClsKpiDescripcionTotal> TotalTicketsPorDep { get; set; }
+        public List<ClsKeyValue> DepartamentoColores { get; set; }
     }
 
     public class TkTotales
@@ -106,8 +161,33 @@ namespace Asiservy.Automatizacion.Formularios.Controllers
         public string Estado { get; set; }
         public string AgenteAsignado { get; set; }
         public string FechaCreacion { get; set; }
+        public string HoraCreacion { get; set; }
+        public string FechaAsignacion { get; set; }
+        public string HoraAsignacion { get; set; }
         public string FechaCierre { get; set; }
-        public int Dias { get; set; }
+        public string HoraCierre { get; set; }
+        public string FechaInicioSoporte { get; set; }
+        public string HoraInicioSoporte { get; set; }
+        public string FechaFinSoporte { get; set; }
+        public string HoraFinSoporte { get; set; }
+        public string TiempoTicket { get; set; }
+        public string TiempoAsignacionCierre { get; set; }
+        public string TiempoSoporte { get; set; }
+        public string TiempoCreacionFinSoporte { get; set; }
+        public string TiempoTicketLetras { get; set; }
+        public int SoporteMinutos { get; set; }
+        public decimal SoporteHoras { get; set; }
+        public decimal SoporteDias { get; set; }
+        public int TicketMinutos { get; set; }
+        public decimal TicketHoras { get; set; }
+        public decimal TicketDias { get; set; }
+        public int AsignacionCierreMinutos { get; set; }
+        public decimal AsignacionCierreHoras { get; set; }
+        public decimal AsignacionCierreDias { get; set; }
+        public int CreacionFinSoporteMinutos { get; set; }
+        public decimal CreacionFinSoporteHoras { get; set; }
+        public decimal CreacionFinSoporteDias { get; set; }
+        public string Color { get; set; }
     }
     
 }
