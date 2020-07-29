a8a369fb956434f8bab57e38c8200af0d6dbfc42
Ing Victor <victorrcch@hotmail.com>
2020-07-29 16:56:11 -0500

    Mantenimiento apariencia sensorial

---
+++ Asiservy.Automatizacion.Formularios/Asiservy.Automatizacion.Formularios/AccesoDatos/CALIDAD/AnalisisSensorial/ClsdMantenimientoApariencia.cs
@@ -0,0 +1,61 @@
+﻿using Asiservy.Automatizacion.Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asiservy.Automatizacion.Formularios.AccesoDatos.CALIDAD.AnalisisSensorial
+{
+    public class ClsdMantenimientoApariencia
+    {
+
+        public List<CC_MANTENIMIENTO_APARIENCIA_AS> ConsultaManteminetoApariencia()
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var lista = entities.CC_MANTENIMIENTO_APARIENCIA_AS.ToList();
+                return lista;
+            }
+        }
+
+        public void GuardarModificarMantenimientoApariencia(CC_MANTENIMIENTO_APARIENCIA_AS model)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var poControl = entities.CC_MANTENIMIENTO_APARIENCIA_AS.FirstOrDefault(x => x.IdApariencia == model.IdApariencia);
+                if (poControl != null)
+                {
+                    poControl.Descripcion = model.Descripcion.ToUpper();
+                    poControl.Abreviatura = model.Abreviatura.ToUpper();
+                    poControl.EstadoRegistro = model.EstadoRegistro;
+                    poControl.TerminalModificacionLog = model.TerminalIngresoLog;
+                    poControl.UsuarioModificacionLog = model.UsuarioIngresoLog;
+                    poControl.FechaModificacionLog = model.FechaIngresoLog;
+                }
+                else
+                {
+                    model.Descripcion = model.Descripcion.ToUpper();
+                    model.Abreviatura = model.Abreviatura.ToUpper();
+                    entities.CC_MANTENIMIENTO_APARIENCIA_AS.Add(model);
+                }
+                entities.SaveChanges();
+            }
+        }
+
+        public void EliminarMantenimientoApariencia(CC_MANTENIMIENTO_APARIENCIA_AS model)
+        {
+            using (ASIS_PRODEntities entities = new ASIS_PRODEntities())
+            {
+                var poControl = entities.CC_MANTENIMIENTO_APARIENCIA_AS.FirstOrDefault(x => x.IdApariencia == model.IdApariencia);
+                if (poControl != null)
+                {
+                    poControl.EstadoRegistro = model.EstadoRegistro;
+                    poControl.TerminalModificacionLog = model.TerminalIngresoLog;
+                    poControl.UsuarioModificacionLog = model.UsuarioIngresoLog;
+                    poControl.FechaModificacionLog = model.FechaIngresoLog;
+                    entities.SaveChanges();
+                }
+
+            }
+        }
+    }
+}
